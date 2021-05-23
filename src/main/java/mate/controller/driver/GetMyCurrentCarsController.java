5f75ea2f151fc29358aebbed9171d396ef9f5b82
safordog <safordog@gmail.com>
2021-05-23 14:58:46 +0300

    added solution

---
+++ src/main/java/mate/controller/driver/GetMyCurrentCarsController.java
@@ -0,0 +1,25 @@
+package mate.controller.driver;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.service.CarService;
+
+@WebServlet(urlPatterns = "/driver/cars")
+public class GetMyCurrentCarsController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final CarService carService = (CarService) injector
+            .getInstance(CarService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Long driverId = (Long) req.getSession().getAttribute("driver_id");
+        req.setAttribute("cars", carService.getAllByDriver(driverId));
+        req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,33 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<style>
+    <%@include file='/WEB-INF/views/css/table_dark.css' %>
+</style>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<form method="post" id="login" action="${pageContext.request.contextPath}/login"></form>
+<h1 class="table_dark">Login page</h1>
+<h4 style="color:red" class="table_dark">${errorMessage}</h4>
+<table border="1" class="table_dark">
+    <tr>
+        <th>Login</th>
+        <th>Password</th>
+        <th>Submit</th>
+    </tr>
+    <tr>
+        <td>
+            <input type="text" name="login" form="login" required>
+        </td>
+        <td>
+            <input type="password" name="password" form="login" required>
+        </td>
+        <td>
+            <input type="submit" name="login" form="login">
+        </td>
+    </tr>
+</table>
+<h5 class="table_dark"><a href="${pageContext.request.contextPath}/drivers/add">Register</a></h5>
+</body>
+</html>
