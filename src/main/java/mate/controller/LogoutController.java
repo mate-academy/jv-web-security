a764b20daf098b5b62999f83a17bedcbf86570b1
dvdovynets <vdovinec@gmail.com>
2021-05-25 17:16:41 +0300

    made suggested changes

--- src/main/java/mate/controller/LogoutController.java
+++ src/main/java/mate/controller/LogoutController.java
@@ -11,6 +11,6 @@ public class LogoutController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         req.getSession().invalidate();
-        resp.sendRedirect("/webapp/WEB-INF/views/index.jsp");
+        resp.sendRedirect("/index.jsp");
     }
 }
