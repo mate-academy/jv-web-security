d2e76d3edfbc36c6e836eb8e5975f68ab8091637
Yevhenii Denysenko <iedenysenko@gmail.com>
2021-05-25 15:48:21 +0300

    resolve some mistakes

--- src/main/java/mate/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -1,7 +1,6 @@
 package mate.service;
 
 import java.util.Optional;
-import mate.dao.DriverDao;
 import mate.lib.Inject;
 import mate.lib.Service;
 import mate.lib.exception.AuthenticationException;
@@ -10,15 +9,12 @@ import mate.model.Driver;
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
-    private DriverDao driverDao;
+    private DriverService driverService;
     
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
-        Optional<Driver> driver = driverDao.findByLogin(login);
-        if (driver.isEmpty()) {
-            throw new AuthenticationException("Login or password was incorrect");
-        }
-        if (driver.get().getPassword().equals(password)) {
+        Optional<Driver> driver = driverService.findByLogin(login);
+        if (driver.isPresent() && driver.get().getPassword().equals(password)) {
             return driver.get();
         }
         throw new AuthenticationException("Login or password was incorrect");
--- src/main/java/mate/web/filter/AuthenticationFilter.java
+++ src/main/java/mate/web/filter/AuthenticationFilter.java
@@ -30,7 +30,7 @@ public class AuthenticationFilter implements Filter {
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         HttpSession session = req.getSession();
         Long driverId = (Long) session.getAttribute(DRIVER_ID);
-        if (driverId == null && allowedUrls.contains(req.getServletPath())) {
+        if (allowedUrls.contains(req.getServletPath())) {
             filterChain.doFilter(req, resp);
             return;
         }
