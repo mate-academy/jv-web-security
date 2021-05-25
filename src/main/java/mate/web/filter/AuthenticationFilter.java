1886870858180e5da8c2dfc564ff6980b9257ff9
Iryna Ustymenko <i.ustimenko18@gmail.com>
2021-05-25 12:50:25 +0300

    fixed after revue

--- src/main/java/mate/web/filter/AuthenticationFilter.java
+++ src/main/java/mate/web/filter/AuthenticationFilter.java
@@ -20,6 +20,7 @@ public class AuthenticationFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
         allowedUrls.add("/login");
         allowedUrls.add("/drivers/add");
+        allowedUrls.add("/index");
     }
 
     @Override
