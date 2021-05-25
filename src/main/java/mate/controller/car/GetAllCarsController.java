e8f5a592e1eb83477f6222bfd3bd84e42f9bd6cb
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-05-25 18:59:38 +0300

    fixed style issues

--- src/main/java/mate/controller/car/GetAllCarsController.java
+++ src/main/java/mate/controller/car/GetAllCarsController.java
@@ -6,21 +6,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import mate.lib.Injector;
-import mate.service.CarService;
 
 @WebServlet(urlPatterns = "/cars/")
 public class GetAllCarsController extends HttpServlet {
-    private static final Injector injector = Injector.getInstance("mate");
-    private final CarService carService = (CarService) injector
-            .getInstance(CarService.class);
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        //List<Car> allCars = carService.getAll();
-        //req.setAttribute("cars", allCars);
-        //req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
         resp.sendRedirect("/cars/all");
     }
 }
