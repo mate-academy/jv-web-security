44b9741b3dd6719335349797343c3dcaedc6a165
Veronika <koraf9712@gmail.com>
2021-05-26 19:24:45 +0300

    Added first solution

---
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -0,0 +1,21 @@
+package mate.service;
+
+import java.util.Optional;
+import mate.lib.Injector;
+import mate.lib.exception.AuthenticationException;
+import mate.model.Driver;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
+    @Override
+    public Driver login(String login, String password) throws AuthenticationException {
+        Optional<Driver> driver = driverService.findByLogin(login);
+        if (driver.isPresent() && driver.get().getPassword().equals(password)) {
+            return driver.get();
+        }
+        throw new AuthenticationException("Login or password was incorrect");
+    }
+}
