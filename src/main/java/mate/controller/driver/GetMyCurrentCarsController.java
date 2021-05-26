5c08c0810049e5b204b19079b0f2870dfb474837
SerKisJD <sergej.kislyakov@gmail.com>
2021-05-26 15:43:20 +0300

    3 fixed servlets

--- src/main/java/mate/controller/driver/GetMyCurrentCarsController.java
+++ src/main/java/mate/controller/driver/GetMyCurrentCarsController.java
@@ -2,7 +2,6 @@ package mate.controller.driver;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpSession;
 import mate.lib.Injector;
 import mate.service.CarService;
 
-@WebServlet(urlPatterns = "/drivers/cars/current")
 public class GetMyCurrentCarsController extends HttpServlet {
     private static final String DRIVER_ID = "driver_id";
     private static final Injector injector = Injector.getInstance("mate");
