c4c37e479a287c04020c49b7027796c544a86ad1
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-05-26 12:05:41 +0300

    controller fix

--- src/main/java/mate/controller/car/GetAllCarsController.java
+++ src/main/java/mate/controller/car/GetAllCarsController.java
@@ -12,6 +12,6 @@ public class GetAllCarsController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.sendRedirect("/cars/all");
+        resp.sendRedirect("/cars/current");
     }
 }
