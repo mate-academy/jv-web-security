711690cd19f78f3d705843a426bd61f98bb6b3da
unknown <vovazalli@gmail.com>
2021-05-27 12:41:19 +0300

    final

--- src/main/java/mate/controller/GetMyCurrentCarsController.java
+++ src/main/java/mate/controller/GetMyCurrentCarsController.java
@@ -1,7 +1,6 @@
 package mate.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import mate.dao.CarDao;
 import mate.lib.Injector;
 import mate.model.Car;
-import mate.model.Driver;
 
 @WebServlet("/cars/current")
 public class GetMyCurrentCarsController extends HttpServlet {
@@ -23,15 +21,7 @@ public class GetMyCurrentCarsController extends HttpServlet {
             throws ServletException, IOException {
         Long driverId = (Long) req.getSession().getAttribute("driver_id");
         List<Car> allByDriver = carDao.getAllByDriver(driverId);
-        String name = (String) req.getSession().getAttribute("driver_name");
-        String licenseNumber = (String) req.getSession().getAttribute("driver_licenseNumber");
-        Driver driver = new Driver(name, licenseNumber);
-        driver.setId(driverId);
-        List<Driver> driverList = new ArrayList<>();
-        driverList.add(driver);
-        for (Car car : allByDriver) {
-            car.setDrivers(driverList);
-        }
+        
         req.setAttribute("cars", allByDriver);
         req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
     }
