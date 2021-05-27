04403806b14a240887a6d3ddc750874d3ea7b2a6
Misha Antonyuk <63130356+the5tranger@users.noreply.github.com>
2021-05-27 17:41:34 +0300

    Update src/main/java/mate/service/AuthenticationServiceImpl.java
    Co-authored-by: Dasha <maliuk.dasha@gmail.com>

--- src/main/java/mate/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -14,7 +14,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public Driver login(String username, String password) throws AuthenticationException {
         Optional<Driver> driver = driverService.findByLogin(username);
-        if (!driver.isEmpty() && driver.get().getPassword().equals(password)) {
+        if (driver.isPresent() && driver.get().getPassword().equals(password)) {
             return driver.get();
         }
         throw new AuthenticationException("Username or password was incorrect");
