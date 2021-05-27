973cf48662378930725a8ba0da6983ac552bce29
Bohdan Borsh <borshbohdan@gmail.com>
2021-05-27 14:25:47 +0300

    HW Done (hopefully).

--- src/main/java/mate/controller/car/GetMyCurrentCarsController.java
+++ src/main/java/mate/controller/car/GetMyCurrentCarsController.java
@@ -12,6 +12,7 @@ import mate.model.Car;
 import mate.service.CarService;
 
 public class GetMyCurrentCarsController extends HttpServlet {
+    public static final String DRIVER_ID = "driver_id";
     private static final Injector injector = Injector.getInstance("mate");
     private final CarService carService = (CarService) injector
             .getInstance(CarService.class);
@@ -20,7 +21,7 @@ public class GetMyCurrentCarsController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         HttpSession httpSession = req.getSession();
-        Long driverId = (Long) httpSession.getAttribute("driver_id");
+        Long driverId = (Long) httpSession.getAttribute(DRIVER_ID);
         List<Car> driverCars = carService.getAllByDriver(driverId);
         req.setAttribute("cars", driverCars);
         req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
