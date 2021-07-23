9f7095516b505884e27ade2857ba35c1a1683ca0
nikdagaev <nikdagaev@gmail.com>
2021-07-23 16:44:02 +0300

    Authentication implemented

--- src/main/java/mate/service/DriverServiceImpl.java
+++ src/main/java/mate/service/DriverServiceImpl.java
@@ -35,4 +35,9 @@ public class DriverServiceImpl implements DriverService {
     public boolean delete(Long id) {
         return driverDao.delete(id);
     }
+
+    public Driver findByLogin(String login) {
+        return driverDao.findByLogin(login).get();
+    }
+
 }
