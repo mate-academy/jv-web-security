e68b85e8f350aebffaf90b4daa587bbcd34ee8a9
Borys Skvortsov <Dhakmar@gmail.com>
2021-07-24 06:14:55 +0300

    Created login.jsp & LoginController.

---
+++ src/main/java/mate/controller/LoginController.java
@@ -0,0 +1,38 @@
+package mate.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.exception.AuthenticationException;
+import mate.lib.Injector;
+import mate.service.AuthenticationService;
+
+@WebServlet(urlPatterns = "/login")
+public class LoginController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final AuthenticationService authenticationService =
+            (AuthenticationService) injector.getInstance(AuthenticationService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+        req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        try {
+            req.getSession().setAttribute("driver", authenticationService.login(login, password));
+            resp.sendRedirect("/");
+        } catch (AuthenticationException e) {
+            req.setAttribute("errorMsg", e.getMessage());
+            req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
+        }
+    }
+}
