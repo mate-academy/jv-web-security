a3a5cda8e2c6e9a6c045f37970584bd9a5ef9489
Veronika-Alekseienko <alekseienko.n@gmail.com>
2021-07-25 01:18:56 +0300

    fixed checkstyle

--- src/main/java/mate/controller/driver/GetMyCurrentCarsController.java
+++ src/main/java/mate/controller/driver/GetMyCurrentCarsController.java
@@ -13,6 +13,7 @@ import mate.service.CarService;
 
 @WebServlet(urlPatterns = "/drivers/cars/get")
 public class GetMyCurrentCarsController extends HttpServlet {
+    private static final String DRIVER_ID = "driver_id";
     private static final Injector injector = Injector.getInstance("mate");
     private final CarService carService =
             (CarService) injector.getInstance(CarService.class);
@@ -20,7 +21,7 @@ public class GetMyCurrentCarsController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        Long driverId = (Long) req.getSession().getAttribute("driver_id");
+        Long driverId = (Long) req.getSession().getAttribute(DRIVER_ID);
         List<Car> allCarsByDriver = carService.getAllByDriver(driverId);
         req.setAttribute("cars", allCarsByDriver);
         req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
--- src/main/java/mate/web/filter/AuthenticationFilter.java
+++ src/main/java/mate/web/filter/AuthenticationFilter.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpSession;
 
 @WebFilter(urlPatterns = "/*")
 public class AuthenticationFilter implements Filter {
+    private static final String DRIVER_ID = "driver_id";
     private Set<String> allowedUrl = new HashSet<>();
 
     @Override
@@ -31,7 +32,7 @@ public class AuthenticationFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         HttpSession session = req.getSession();
-        Long driverId = (Long) session.getAttribute("driver_id");
+        Long driverId = (Long) session.getAttribute(DRIVER_ID);
         if (driverId == null && !allowedUrl.contains(req.getServletPath())) {
             resp.sendRedirect("/login");
             return;
