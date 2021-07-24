10e5ba8037d2cc03fd50a485e78a5f27a48f2dbf
Benito <zara.benito17@gmail.com>
2021-07-25 01:46:40 +0300

    fixed checkstyle again;

--- src/main/java/mate/controller/driver/GetMyCurrentCarsController.java
+++ src/main/java/mate/controller/driver/GetMyCurrentCarsController.java
@@ -10,8 +10,9 @@ import javax.servlet.http.HttpSession;
 import mate.lib.Injector;
 import mate.service.CarService;
 
-@WebServlet(urlPatterns = "/cars/by_id")
+@WebServlet(urlPatterns = "/drivers/cars")
 public class GetMyCurrentCarsController extends HttpServlet {
+    private static final String DRIVER_ID = "driver_id";
     private static final Injector injector = Injector.getInstance("mate");
     private CarService carService
             = (CarService) injector.getInstance(CarService.class);
@@ -20,7 +21,7 @@ public class GetMyCurrentCarsController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         HttpSession session = req.getSession();
-        Long driverId = (Long) session.getAttribute("driver_id");
+        Long driverId = (Long) session.getAttribute(DRIVER_ID);
         req.setAttribute("cars", carService.getAllByDriver(driverId));
         req.getRequestDispatcher("/WEB-INF/views/cars.jsp").forward(req, resp);
     }
