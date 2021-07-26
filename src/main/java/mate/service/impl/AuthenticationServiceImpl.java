846d209df3a0f629e9ee36a43674a4141c9c3660
vKutik <84785564+vKutik@users.noreply.github.com>
2021-07-26 18:26:25 +0300

    remake update add inject Servise

--- src/main/java/mate/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/service/impl/AuthenticationServiceImpl.java
@@ -3,21 +3,20 @@ package mate.service.impl;
 import java.util.Optional;
 import mate.dao.DriverDao;
 import mate.exception.AuthenticationException;
+import mate.lib.Inject;
 import mate.lib.Injector;
 import mate.model.Driver;
 import mate.service.AuthenticationService;
+import mate.service.DriverService;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
-    private Injector injector = Injector.getInstance("mate");
-    private DriverDao driverDao = (DriverDao) injector.getInstance(DriverDao.class);
+    @Inject
+    private DriverService driverService;
 
     @Override
     public Driver login(String username, String password) throws AuthenticationException {
-        Optional<Driver> user = driverDao.findByDriverLogin(username);
-        if (user.isEmpty()) {
-            throw new AuthenticationException("Login or password was incorrect");
-        }
-        if (user.get().getPassword().equals(password)) {
+        Optional<Driver> user = driverService.findByDriverLogin(username);
+        if (!user.isEmpty() && user.get().getPassword().equals(password)) {
             return user.get();
         }
         throw new AuthenticationException("Login or password was incorrect");
