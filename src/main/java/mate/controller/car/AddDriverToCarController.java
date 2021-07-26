e9ac581367b5ee54e9e493daea69fd1c595f3b13
kseniia_makarova <kseniia.makarova.kyiv@gmail.com>
2021-07-26 12:54:21 +0300

    fixed mistakes - mainly column names, changed the link to the checklist

--- src/main/java/mate/controller/car/AddDriverToCarController.java
+++ src/main/java/mate/controller/car/AddDriverToCarController.java
@@ -26,7 +26,7 @@ public class AddDriverToCarController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException, ServletException {
+            throws IOException {
         long driverId = Long.parseLong(req.getParameter("driver_id"));
         long carId = Long.parseLong(req.getParameter("car_id"));
         Driver driver = driverService.get(driverId);
--- src/main/java/mate/controller/driver/AddDriverController.java
+++ src/main/java/mate/controller/driver/AddDriverController.java
@@ -22,10 +22,10 @@ public class AddDriverController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException, ServletException {
+            throws IOException {
         String name = req.getParameter("name");
-        String licenceNumber = req.getParameter("licence_number");
-        Driver driver = new Driver(name, licenceNumber);
+        String licenseNumber = req.getParameter("license_number");
+        Driver driver = new Driver(name, licenseNumber);
         driverService.create(driver);
         resp.sendRedirect("/drivers/add");
     }
