23502e0e0ac4d0ebbced7406fd8c040b77f14347
Vitalii Martynov <VitaliiMvDeveloper@gmail.com>
2021-09-06 12:37:17 +0300

    Initial solution

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -5,11 +5,26 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import mate.lib.Injector;
+import mate.service.DriverService;
 
 public class IndexController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Long userId = (Long) session.getAttribute("userId");
+        if (userId == null) {
+            resp.sendRedirect("/login");
+            return;
+        }
+        String userName = (String) session.getAttribute("userName");
+        req.setAttribute("userName", (userName.isEmpty()) ? "pal" : userName);
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
