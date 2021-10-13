7ab7995dc917959dab8b02cd3d38d8710139c4aa
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-10-13 21:24:02 +0300

    fixed web.xml

--- src/main/java/mate/controller/car/GetMyCurrentCarsController.java
+++ src/main/java/mate/controller/car/GetMyCurrentCarsController.java
@@ -3,7 +3,6 @@ package mate.controller.car;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +10,6 @@ import mate.lib.Injector;
 import mate.model.Car;
 import mate.service.CarService;
 
-@WebServlet(urlPatterns = "/cars/logged")
 public class GetMyCurrentCarsController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final CarService carService = (CarService) injector
