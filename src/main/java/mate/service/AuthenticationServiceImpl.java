a5138b82b55d9ba7afe7c19b8d97aee0fc447a95
Serhii Blakh <s.a.blakh@ukr.net>
2021-10-14 22:21:06 +0300

    Added AuthenticationService, AuthenticationFilter and LoginController

---
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -0,0 +1,26 @@
+package mate.service;
+
+import java.util.Optional;
+import mate.exception.AuthenticationException;
+import mate.lib.Injector;
+import mate.lib.Service;
+import mate.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService
+            = (DriverService) injector.getInstance(DriverService.class);
+
+    @Override
+    public Driver login(String login, String password) throws AuthenticationException {
+        Optional<Driver> driver = driverService.findDriverByLogin(login);
+        if (driver.isEmpty()) {
+            throw new AuthenticationException("Login or password was incorrect");
+        }
+        if (driver.get().getPassword().equals(password)) {
+            return driver.get();
+        }
+        throw new AuthenticationException("Login or password was incorrect");
+    }
+}
