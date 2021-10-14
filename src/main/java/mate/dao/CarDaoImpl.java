b04a37b932c0400448b93098d45bfe57333d2a23
Andrii Demchenko <avd998ne@gmail.com>
2021-10-14 14:30:41 +0300

    changed DB connection parameters, added login and password support in controllers and .jsp files

--- src/main/java/mate/dao/CarDaoImpl.java
+++ src/main/java/mate/dao/CarDaoImpl.java
@@ -224,9 +224,11 @@ public class CarDaoImpl implements CarDao {
 
     private Driver parseDriverFromResultSet(ResultSet resultSet) throws SQLException {
         Long driverId = resultSet.getObject("id", Long.class);
-        String name = resultSet.getNString("name");
+        String name = resultSet.getString("name");
+        String login = resultSet.getString("login");
+        String password = resultSet.getString("password");
         String licenseNumber = resultSet.getNString("license_number");
-        Driver driver = new Driver(name, licenseNumber);
+        Driver driver = new Driver(name, login, password, licenseNumber);
         driver.setId(driverId);
         return driver;
     }
--- src/main/webapp/WEB-INF/views/drivers/all.jsp
+++ src/main/webapp/WEB-INF/views/drivers/all.jsp
@@ -13,6 +13,7 @@
     <tr>
         <th>ID</th>
         <th>Name</th>
+        <th>Login</th>
         <th>License number</th>
         <th>Delete</th>
     </tr>
@@ -23,6 +24,9 @@
             </td>
             <td>
                 <c:out value="${driver.name}"/>
+            </td>
+            <td>
+                <c:out value="${driver.login}"/>
             </td>
             <td>
                 <c:out value="${driver.licenseNumber}"/>
