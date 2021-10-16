3a1b920396f635a99d7d8d03ad51344d5c718629
SolnalarmJulia <solnalarm@meta.ua>
2021-10-17 01:24:44 +0300

    refactoring

--- src/main/java/mate/web/filter/AuthenticationFilter.java
+++ src/main/java/mate/web/filter/AuthenticationFilter.java
@@ -29,11 +29,7 @@ public class AuthenticationFilter implements Filter {
         HttpServletResponse resp = (HttpServletResponse) response;
         HttpSession session = req.getSession();
         Long driverId = (Long) session.getAttribute("driver_id");
-        if (driverId == null && allowedUrl.contains(req.getServletPath())) {
-            filterChain.doFilter(req,resp);
-            return;
-        }
-        if (driverId == null) {
+        if (driverId == null && !allowedUrl.contains(req.getServletPath())) {
             resp.sendRedirect("/login");
             return;
         }
