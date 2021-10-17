75f009c0efe280b5ccb159dec89ed64250e62fcf
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-10-17 22:24:05 +0300

    refactored jsp files

--- src/main/java/mate/dao/DriverDaoImpl.java
+++ src/main/java/mate/dao/DriverDaoImpl.java
@@ -75,7 +75,7 @@ public class DriverDaoImpl implements DriverDao {
     @Override
     public Driver update(Driver driver) {
         String query = "UPDATE drivers "
-                + "SET name = ?, license_number = ? "
+                + "SET name = ?, license_number = ?, "
                 + "login = ?, password = ?"
                 + "WHERE id = ? AND is_deleted = FALSE";
         try (Connection connection = ConnectionUtil.getConnection();
