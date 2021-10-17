6664a02d41410e09c4b4b0715c60412f5e1e14df
Andrii Demchenko <87430083+andrii-demchenko@users.noreply.github.com>
2021-10-17 21:37:42 +0300

    changed constant access modifier to private
    Co-authored-by: Artem Lavrivskyi <85931447+lavrivskyi@users.noreply.github.com>

--- src/main/java/mate/controller/driver/GetMyCurrentCarsController.java
+++ src/main/java/mate/controller/driver/GetMyCurrentCarsController.java
@@ -12,7 +12,7 @@ import mate.model.Car;
 import mate.service.CarService;
 
 public class GetMyCurrentCarsController extends HttpServlet {
-    public static final String DRIVER_ID = "driver_id";
+    private static final String DRIVER_ID = "driver_id";
     private static final Injector injector = Injector.getInstance("mate");
     private final CarService carService = (CarService) injector
             .getInstance(CarService.class);
