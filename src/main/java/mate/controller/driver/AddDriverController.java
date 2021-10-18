0def5761958b966bbf62b50ffee8b17114ae7d71
pustoj0 <pustoj0@gmail.com>
2021-10-18 04:14:46 +0300

    added two fields login and password + modification to Driver layers and jsp page

--- src/main/java/mate/controller/driver/AddDriverController.java
+++ src/main/java/mate/controller/driver/AddDriverController.java
@@ -5,6 +5,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import mate.lib.Injector;
 import mate.model.Driver;
 import mate.service.DriverService;
@@ -25,7 +27,9 @@ public class AddDriverController extends HttpServlet {
             throws IOException {
         String name = req.getParameter("name");
         String licenseNumber = req.getParameter("license_number");
-        Driver driver = new Driver(name, licenseNumber);
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        Driver driver = new Driver(name, licenseNumber, login, password);
         driverService.create(driver);
         resp.sendRedirect("/drivers/add");
     }
