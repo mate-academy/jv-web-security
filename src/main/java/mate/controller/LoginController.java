b04e5da821880755b4e92b296277ff1af45d9700
Dmytro Heleta <dmytroheleta@gmail.com>
2021-10-18 13:08:41 +0300

    removed redundant constant

--- src/main/java/mate/controller/LoginController.java
+++ src/main/java/mate/controller/LoginController.java
@@ -12,7 +12,6 @@ import mate.model.Driver;
 import mate.service.AuthenticationService;
 
 public class LoginController extends HttpServlet {
-    private static final String DRIVER_ID = "driver_id";
     private static final Injector injector = Injector.getInstance("mate");
     private final AuthenticationService authenticationService
             = (AuthenticationService) injector.getInstance(AuthenticationService.class);
@@ -32,7 +31,7 @@ public class LoginController extends HttpServlet {
         try {
             Driver driver = authenticationService.login(login, password);
             HttpSession session = req.getSession();
-            session.setAttribute(DRIVER_ID, driver.getId());
+            session.setAttribute("driver_id", driver.getId());
             resp.sendRedirect("/index");
         } catch (AuthenticationException e) {
             req.setAttribute("error_message", e.getMessage());
