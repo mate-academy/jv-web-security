c387221f2ffead543dca9341bf042d6d9b2644bf
pustoj0 <pustoj0@gmail.com>
2021-10-18 04:34:12 +0300

    Created new AuthenticationService

---
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -0,0 +1,10 @@
+package mate.service;
+
+import mate.model.Driver;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Override
+    public Driver login(String login, String password) {
+        return null;
+    }
+}
