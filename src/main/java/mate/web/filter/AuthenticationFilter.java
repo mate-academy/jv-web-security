9c412ae63ba10d15c6f1ad6d940e98daa3e12473
forsfakel <81535727+forsfakel@users.noreply.github.com>
2021-12-07 21:19:19 +0200

    correct initialization list public pages in the authentication filter; correct a drivers/add page (change input type from text to password)

--- src/main/java/mate/web/filter/AuthenticationFilter.java
+++ src/main/java/mate/web/filter/AuthenticationFilter.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -13,6 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class AuthenticationFilter implements Filter {
+    private Set<String> links = new HashSet<>();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        links.add("/login");
+        links.add("/drivers/add");
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
@@ -21,9 +29,6 @@ public class AuthenticationFilter implements Filter {
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         HttpSession session = req.getSession();
         Long driverId = (Long) session.getAttribute("driver_id");
-        Set<String> links = new HashSet<>();
-        links.add("/login");
-        links.add("/drivers/add");
         if (driverId == null && !links.contains(req.getServletPath())) {
             resp.sendRedirect("/login");
             return;
