4ce5c2234e05d60ce5d8d2b2963f35705dac7b16
Konstantin <www.kosta.kon@gmail.com>
2021-12-10 16:28:28 +0200

    Method login was refactored.

--- src/main/java/mate/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -14,10 +14,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
         Optional<Driver> optionalDriver = driverService.findDriverByLogin(login);
-        if (optionalDriver.isEmpty()) {
-            throw new AuthenticationException("Login or password was incorrect.");
-        }
-        if (!optionalDriver.get().getPassword().equals(password)) {
+        if (optionalDriver.isEmpty() || !optionalDriver.get().getPassword().equals(password)) {
             throw new AuthenticationException("Login or password was incorrect.");
         }
         return optionalDriver.get();
