0ba8eaa7f335464194b66d8029cb7d94fa807e05
Roman Polulikh <polilikh.r@gmail.com>
2021-12-13 15:43:39 +0200

    implemented AuthenticationService

--- src/main/java/mate/model/Driver.java
+++ src/main/java/mate/model/Driver.java
@@ -6,10 +6,23 @@ public class Driver {
     private Long id;
     private String name;
     private String licenseNumber;
+    private String login;
+    private String password;
 
     public Driver(String name, String licenseNumber) {
         this.name = name;
         this.licenseNumber = licenseNumber;
+    }
+
+    public Driver(String name, String licenseNumber, String login, String password) {
+        this(name, licenseNumber);
+        this.login = login;
+        this.password = password;
+    }
+
+    public Driver(Long id, String name, String licenseNumber, String login, String password) {
+        this(name, licenseNumber, login, password);
+        this.id = id;
     }
 
     public Long getId() {
@@ -34,6 +47,22 @@ public class Driver {
 
     public void setLicenseNumber(String licenseNumber) {
         this.licenseNumber = licenseNumber;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
