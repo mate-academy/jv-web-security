c7776c12bed712fb00ce591da13912bb23933089
Roman Polulikh <polilikh.r@gmail.com>
2021-12-13 19:52:15 +0200

    fixed codeStyle issues

--- src/main/java/mate/controller/LoginController.java
+++ src/main/java/mate/controller/LoginController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import mate.exception.AuthenticationException;
 import mate.lib.Injector;
 import mate.model.Driver;
