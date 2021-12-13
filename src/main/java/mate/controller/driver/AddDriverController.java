60ddde53fd68e9c4ff196a3e17d34a2fd281f95c
Dzmitry Kozel <dmitrokoz88@gmail.com>
2021-12-13 23:17:21 +0200

    login bug fixed;

--- src/main/java/mate/controller/driver/AddDriverController.java
+++ src/main/java/mate/controller/driver/AddDriverController.java
@@ -29,6 +29,6 @@ public class AddDriverController extends HttpServlet {
         String password = req.getParameter("password");
         Driver driver = new Driver(name, licenseNumber, login, password);
         driverService.create(driver);
-        resp.sendRedirect("/drivers/add");
+        resp.sendRedirect("/login");
     }
 }
