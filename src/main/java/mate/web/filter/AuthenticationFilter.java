6e1bb3bfb234e898e8124a5d50906b52bedcd064
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-12-14 15:15:34 +0200

    remove redunt line

--- src/main/java/mate/web/filter/AuthenticationFilter.java
+++ src/main/java/mate/web/filter/AuthenticationFilter.java
@@ -30,12 +30,10 @@ public class AuthenticationFilter implements Filter {
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         HttpSession session = req.getSession();
         Long driverId = (Long) session.getAttribute("driver_id");
-
         if (driverId == null && !allowedUrls.contains(req.getServletPath())) {
             filterChain.doFilter(req, resp);
             return;
         }
-
         filterChain.doFilter(req, resp);
     }
 }
