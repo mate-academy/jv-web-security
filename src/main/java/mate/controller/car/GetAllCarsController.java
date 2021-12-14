a508d160ba2eaabdbea7b8dc9a3d8436d9710542
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-12-14 13:15:45 +0200

    first solution version

--- src/main/java/mate/controller/car/GetAllCarsController.java
+++ src/main/java/mate/controller/car/GetAllCarsController.java
@@ -3,6 +3,7 @@ package mate.controller.car;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ import mate.lib.Injector;
 import mate.model.Car;
 import mate.service.CarService;
 
+@WebServlet("/cars/")
 public class GetAllCarsController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final CarService carService = (CarService) injector
--- src/main/java/mate/controller/driver/DeleteDriverController.java
+++ src/main/java/mate/controller/driver/DeleteDriverController.java
@@ -1,12 +1,14 @@
 package mate.controller.driver;
 
 import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.lib.Injector;
 import mate.service.DriverService;
 
+@WebServlet("/drivers/delete")
 public class DeleteDriverController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final DriverService driverService = (DriverService) injector
