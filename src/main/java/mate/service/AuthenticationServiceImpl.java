4420da24660abbe6498bf595b9741a91a5286434
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-12-14 14:36:17 +0200

    build success

---
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -0,0 +1,23 @@
+package mate.service;
+
+import java.util.Optional;
+import mate.exception.AuthenticationException;
+import mate.lib.Inject;
+import mate.lib.Service;
+import mate.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private DriverService driverService;
+
+    @Override
+    public Driver login(String login, String password) throws AuthenticationException {
+        Optional<Driver> driver = driverService.findByLogin(login);
+        if (driver.isEmpty() || !driver.get().getPassword().equals(password)) {
+            throw new AuthenticationException("Couldn't authenticate driver, incorrect "
+                    + "login or password");
+        }
+        return driver.get();
+    }
+}
