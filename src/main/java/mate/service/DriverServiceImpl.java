9d82fce2e17185e0cb9ac66cac2c4566fc137823
Anatolii Huryk <tolik.mario96@gmail.com>
2021-12-14 12:53:11 +0200

    fix

--- src/main/java/mate/service/DriverServiceImpl.java
+++ src/main/java/mate/service/DriverServiceImpl.java
@@ -39,6 +39,6 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public Optional<Driver> findByLogin(String login) {
-        return Optional.empty();
+        return driverDao.findByLogin(login);
     }
 }
