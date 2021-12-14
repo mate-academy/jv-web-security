e075f0809a8365f4f8b212b92eab3023e309ad6d
Yevheniy Savenkov <yevheniy.savenkov@gmail.com>
2021-12-14 14:12:30 +0200

    if blocks in AuthenticationFilter simplified

--- src/main/java/mate/web/filter/AuthenticationFilter.java
+++ src/main/java/mate/web/filter/AuthenticationFilter.java
@@ -31,14 +31,10 @@ public class AuthenticationFilter implements Filter {
         HttpSession session = req.getSession();
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         Long driverId = (Long) session.getAttribute("id");
-        if (driverId == null && allowedUrls.contains(req.getServletPath())) {
+        if (driverId != null || allowedUrls.contains(req.getServletPath())) {
             filterChain.doFilter(req, resp);
             return;
         }
-        if (driverId == null) {
-            resp.sendRedirect("/login");
-            return;
-        }
-        filterChain.doFilter(req, resp);
+        resp.sendRedirect("/login");
     }
 }
