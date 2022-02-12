96d1509ce154c132fdf2e950f70467e415ad2991
Roman Okhrymovych <okhrymovych11@gmail.com>
2022-02-12 23:04:13 +0200

    Authentication Security Commit

--- src/main/java/taxi/model/Driver.java
+++ src/main/java/taxi/model/Driver.java
@@ -6,8 +6,18 @@ public class Driver {
     private Long id;
     private String name;
     private String licenseNumber;
+    private String login;
+    private String password;
 
     public Driver() {
+    }
+
+    public Driver(String name, String licenseNumber, String login) {
+    }
+
+    public Driver(String name, String licenseNumber, String login, String password) {
+        this(name, licenseNumber,login);
+        this.password = password;
     }
 
     public Driver(String name, String licenseNumber) {
@@ -39,6 +49,22 @@ public class Driver {
         this.licenseNumber = licenseNumber;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -56,5 +82,15 @@ public class Driver {
     @Override
     public int hashCode() {
         return Objects.hash(id, name, licenseNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", licenseNumber='" + licenseNumber + '\''
+                + ", login='" + login + '\''
+                + '}';
     }
 }
