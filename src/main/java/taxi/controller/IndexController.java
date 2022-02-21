1bca1794041bb4ae4a712471c676d7619f9b9811
sviat <svyatoslav.grinik@gmail.com>
2022-02-21 21:41:43 +0200

    added display of current driver's name in index page instead of 'mates'

--- src/main/java/taxi/controller/IndexController.java
+++ src/main/java/taxi/controller/IndexController.java
@@ -5,11 +5,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
 
 public class IndexController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        Long currentDriverId = (Long) req.getSession().getAttribute("driver_id");
+        Driver driver = driverService.get(currentDriverId);
+        req.setAttribute("currentDriverName", driver.getName());
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
