aa5fb011e95e80fb74ab354c304c144cb4d5a27b
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-03-28 13:40:21 +0300

    Added logistics errors in RegisterController

--- src/main/java/taxi/controller/authentication/RegisterController.java
+++ src/main/java/taxi/controller/authentication/RegisterController.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class RegisterController extends HttpServlet {
@@ -29,7 +30,13 @@ public class RegisterController extends HttpServlet {
         String confirmPassword = req.getParameter("confirmPassword");
         if (!password.equals(confirmPassword)) {
             req.setAttribute("errorMsg", "Passwords do not match.");
-            req.getRequestDispatcher("/WEB-INF/views/authentication/register.jsp").forward(req, resp);
+            HttpSession session = req.getSession();
+            Long driverId = (Long) session.getAttribute("driver_id");
+            if (driverId == null) {
+                req.getRequestDispatcher("/WEB-INF/views/authentication/register.jsp").forward(req, resp);
+            } else {
+                req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+            }
             return;
         }
         String name = req.getParameter("name");
