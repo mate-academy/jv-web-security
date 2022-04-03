d07473ae6fd8d1fbcf496fa43e34150920716ab7
Ostap Danyliv <danyliv.ostap@gmail.com>
2022-04-03 14:28:17 +0300

    Web Security

---
+++ src/main/java/taxi/controller/authentication/CreateDriverController.java
@@ -0,0 +1,50 @@
+package taxi.controller.authentication;
+
+import java.io.IOException;
+import java.util.Optional;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
+
+@WebServlet(urlPatterns = "/register")
+public class CreateDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final DriverService driverService = (DriverService)
+            injector.getInstance(DriverService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/authentication/register.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Driver driver = new Driver();
+        driver.setName(req.getParameter("driver_name"));
+        driver.setLicenseNumber(req.getParameter("license"));
+        driver.setLogin(req.getParameter("driver_login"));
+        Optional<Driver> driverFromDb = driverService.findByLogin(req.getParameter("driver_login"));
+        if (driverFromDb.isPresent()) {
+            req.setAttribute("errorMsg", "Driver with login: '"
+                    + req.getParameter("driver_login") + "' already exists");
+            req.getRequestDispatcher("/WEB-INF/views/authentication/register.jsp")
+                    .forward(req, resp);
+        }
+        if (req.getParameter("driver_password").equals(req.getParameter("password_repeat"))) {
+            driver.setPassword(req.getParameter("driver_password"));
+            driverService.create(driver);
+            resp.sendRedirect("/login");
+        } else {
+            req.setAttribute("errorMsg", "Passwords are not the same");
+            req.getRequestDispatcher("/WEB-INF/views/authentication/register.jsp")
+                    .forward(req, resp);
+        }
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/header.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<a href="${pageContext.request.contextPath}/logout">Logout</a>
+<a href="${pageContext.request.contextPath}/drivers/allCars">My Cars</a>
+<a href="${pageContext.request.contextPath}/Index">Main</a>
+</body>
+</html>
