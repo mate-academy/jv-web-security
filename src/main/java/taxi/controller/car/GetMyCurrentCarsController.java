65a896af4dc48262938b6c23818c5f0793abf68a
Liubov Chernohor <liubovchernohor@gmail.com>
2022-04-04 16:34:48 +0300

    stile mistakes were fixed

--- src/main/java/taxi/controller/car/GetMyCurrentCarsController.java
+++ src/main/java/taxi/controller/car/GetMyCurrentCarsController.java
@@ -1,15 +1,14 @@
 package taxi.controller.car;
 
-import taxi.lib.Injector;
-import taxi.model.Car;
-import taxi.service.CarService;
-
+import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
+import taxi.lib.Injector;
+import taxi.model.Car;
+import taxi.service.CarService;
 
 public class GetMyCurrentCarsController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
@@ -17,7 +16,8 @@ public class GetMyCurrentCarsController extends HttpServlet {
             .getInstance(CarService.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         List<Car> allCars = carService.getAllByDriver((Long) req.getSession()
                 .getAttribute("driver_id"));
         req.setAttribute("cars", allCars);
