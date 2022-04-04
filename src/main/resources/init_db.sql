2fe65953aec4f8719127281f0cdb4add68f37e5f
Liubov Chernohor <liubovchernohor@gmail.com>
2022-04-04 15:47:01 +0300

    Driver authentication was added

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,25 +7,16 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for drivers
 -- ----------------------------
 DROP TABLE IF EXISTS `drivers`;
-CREATE TABLE `drivers`  (
-                            `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT,
-                            `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                            `license_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                            `is_deleted` bit(1) NOT NULL DEFAULT b'0',
-                            PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for manufacturers
--- ----------------------------
-DROP TABLE IF EXISTS `manufacturers`;
-CREATE TABLE `manufacturers`  (
-                                  `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT,
-                                  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                                  `country` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                                  `is_deleted` bit(1) NOT NULL DEFAULT b'0',
-                                  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `drivers` (
+                           `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                           `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                           `license_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                           `is_deleted` bit(1) NOT NULL DEFAULT b'0',
+                           `login` varchar(45) DEFAULT NULL,
+                           `password` varchar(45) DEFAULT NULL,
+                           PRIMARY KEY (`id`) USING BTREE,
+                           UNIQUE KEY `logon_UNIQUE` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for cars
