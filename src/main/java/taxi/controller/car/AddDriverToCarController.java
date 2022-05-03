09cc4e949f2e57f50a6d776a3f0dec2b03646132
Dmytro Skarzhynets <d.skarzh@protonmail.com>
2022-05-03 08:46:01 +0300

    WebSecurity HW Skarzhynets

--- src/main/java/taxi/controller/car/AddDriverToCarController.java
+++ src/main/java/taxi/controller/car/AddDriverToCarController.java
@@ -13,20 +13,18 @@ import taxi.service.DriverService;
 
 public class AddDriverToCarController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
-    private final CarService carService = (CarService) injector
-            .getInstance(CarService.class);
+    private final CarService carService = (CarService) injector.getInstance(CarService.class);
     private final DriverService driverService = (DriverService) injector
             .getInstance(DriverService.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/views/cars/drivers/add.jsp").forward(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         long driverId = Long.parseLong(req.getParameter("driver_id"));
         long carId = Long.parseLong(req.getParameter("car_id"));
         Driver driver = driverService.get(driverId);
