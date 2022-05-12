726988b2bcae050cf2134bb1df0c571e1606e296
Angela1608 <89303267+Angela1608@users.noreply.github.com>
2022-05-12 16:30:57 +0300

    solved jv-web-security task

--- src/main/java/taxi/web/filter/AuthenticationFilter.java
+++ src/main/java/taxi/web/filter/AuthenticationFilter.java
@@ -19,7 +19,7 @@ public class AuthenticationFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) {
         allowedUrls.add("/login");
-        allowedUrls.add("/register");
+        allowedUrls.add("/registration");
     }
 
     @Override
