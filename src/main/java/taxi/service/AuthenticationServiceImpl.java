4a43fb192d7387a3a3770c59f103e8a3357fc3a4
Mark Shapka <mark.shapka1@gmail.com>
2022-05-18 11:36:56 +0300

    task web-security - created AuthenticationService() - added header.jsp, login.jsp pages - implemented filter based on HttpSession

---
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -0,0 +1,25 @@
+package taxi.service;
+
+import java.util.Optional;
+import taxi.exception.AuthenticationException;
+import taxi.lib.Inject;
+import taxi.lib.Service;
+import taxi.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private DriverService driverService;
+
+    @Override
+    public Driver login(String login, String password) throws AuthenticationException {
+        Optional<Driver> driverFromDB = driverService.findByLogin(login);
+        if (driverFromDB.isEmpty()) {
+            throw new AuthenticationException("Login or password are incorrect");
+        }
+        if (driverFromDB.get().getPassword().equals(password)) {
+            return driverFromDB.get();
+        }
+        throw new AuthenticationException("Login or password are incorrect");
+    }
+}
