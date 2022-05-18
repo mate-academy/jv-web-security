4143668f9fc4b6cc22644d3d23f4a2c03356db7c
Vadym Datsiuk <lutskvadym@gmail.com>
2022-05-18 12:50:27 +0300

    for approving, checked

---
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -0,0 +1,23 @@
+package taxi.service;
+
+import taxi.exception.AuthenticationException;
+import taxi.lib.Inject;
+import taxi.lib.Service;
+import taxi.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private DriverService driverService;
+
+    @Override
+    public Driver login(String login, String password) throws AuthenticationException {
+        Driver driver = driverService.findByLogin(login)
+                .orElseThrow(() -> new AuthenticationException("Could not get driver from DAO "
+                        + "by login = " + login));
+        if (driver.getPassword().equals(password)) {
+            return driver;
+        }
+        throw new AuthenticationException("Login or password is incorrect");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/header.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Header</title>
+</head>
+<body>
+<a href="${pageContext.request.contextPath}/logout">Logout</a>
+</body>
+</html>
