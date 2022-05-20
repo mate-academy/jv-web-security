6ab54e29f37f11385a3bcd04cf0eb23cd409d52d
VladyslavLn <kapuuustin@gmail.com>
2022-05-20 19:55:14 +0300

    Added solution :)

---
+++ src/main/java/taxi/exception/AuthenticationServiceException.java
@@ -0,0 +1,7 @@
+package taxi.exception;
+
+public class AuthenticationServiceException extends Exception {
+    public AuthenticationServiceException(String message) {
+        super(message);
+    }
+}
