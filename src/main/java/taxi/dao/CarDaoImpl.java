d9fdb2f6e07f3b604bf0247fb12f029a66d7e0ce
mordast7 <mordast7@gmail.com>
2022-05-24 14:02:47 +0300

    Modified DriverDaoImpl and CarDaoImpl to fetch all columns for drivers, including new ones.

--- src/main/java/taxi/dao/CarDaoImpl.java
+++ src/main/java/taxi/dao/CarDaoImpl.java
@@ -136,6 +136,8 @@ public class CarDaoImpl implements CarDao {
                 + "manufacturer_id, "
                 + "m.name AS manufacturer_name, "
                 + "m.country AS manufacturer_country "
+                + "d.login AS driver_login"
+                + "d.password AS driver_password"
                 + "FROM cars c"
                 + " JOIN manufacturers m ON c.manufacturer_id = m.id"
                 + " JOIN cars_drivers cd ON c.id = cd.car_id"
@@ -204,7 +206,7 @@ public class CarDaoImpl implements CarDao {
     }
 
     private List<Driver> getAllDriversByCarId(Long carId) {
-        String selectQuery = "SELECT id, name, license_number "
+        String selectQuery = "SELECT id, name, license_number, login, password "
                 + "FROM cars_drivers cd "
                 + "JOIN drivers d ON cd.driver_id = d.id "
                 + "WHERE car_id = ? AND is_deleted = false";
@@ -227,10 +229,14 @@ public class CarDaoImpl implements CarDao {
         Long driverId = resultSet.getObject("id", Long.class);
         String name = resultSet.getNString("name");
         String licenseNumber = resultSet.getNString("license_number");
+        String login = resultSet.getNString("login");
+        String password = resultSet.getNString("password");
         Driver driver = new Driver();
         driver.setId(driverId);
         driver.setName(name);
         driver.setLicenseNumber(licenseNumber);
+        driver.setLogin(login);
+        driver.setPassword(password);
         return driver;
     }
 
