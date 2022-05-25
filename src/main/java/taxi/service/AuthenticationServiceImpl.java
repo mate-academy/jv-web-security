f59356cdc6992bb1bdd00973be01cb556c187f2c
Oleksander <ghjwtyrj25@gmail.com>
2022-05-25 10:08:37 +0300

    Created Authentication for Web-app

---
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -0,0 +1,26 @@
+package taxi.service;
+
+import java.util.Optional;
+import javax.naming.AuthenticationException;
+import taxi.dao.DriverDao;
+import taxi.lib.Inject;
+import taxi.lib.Service;
+import taxi.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private DriverDao driverDao;
+
+    @Override
+    public Driver login(String login, String password) throws AuthenticationException {
+        Optional<Driver> driver = driverDao.findByLogin(login);
+        if (driver.isEmpty()) {
+            throw new AuthenticationException("Login or Password was incorrect!");
+        }
+        if (driver.get().getPassword().equals(password)) {
+            return driver.get();
+        }
+        throw new AuthenticationException("Login or Password was incorrect!");
+    }
+}
