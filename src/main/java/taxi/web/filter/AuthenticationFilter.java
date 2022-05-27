3d534530012e7b3c8fe6201110a5c2536bacb1e6
bukoz <bukoz@meta.ua>
2022-05-27 15:06:39 +0300

    added webservlets

--- src/main/java/taxi/web/filter/AuthenticationFilter.java
+++ src/main/java/taxi/web/filter/AuthenticationFilter.java
@@ -9,10 +9,12 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebFilter(urlPatterns = "/*")
 public class AuthenticationFilter implements Filter {
     private final Set<String> allowedUrl = new HashSet<>();
 
@@ -31,7 +33,7 @@ public class AuthenticationFilter implements Filter {
         HttpSession session = req.getSession();
         Long driverId = (Long) session.getAttribute("driver_id");
         if (driverId == null && !allowedUrl.contains(req.getServletPath())) {
-            resp.sendRedirect(req.getContextPath() + "/login");
+            resp.sendRedirect("/login");
             return;
         }
         filterChain.doFilter(req, resp);
