ccd32eeb5c89321f3673fbaa256cf2669da55d2d
Mykhailo Nikolov <mikenikolov.work@gmail.com>
2022-07-13 19:29:53 +0300

    feat: authentication, cars of a logged in driver

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,58 +1,67 @@
-CREATE SCHEMA IF NOT EXISTS `taxi` DEFAULT CHARACTER SET utf8;
-USE `taxi`;
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
+CREATE DATABASE taxi_service_db
+    WITH
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Ukrainian_Ukraine.1251'
+    LC_CTYPE = 'Ukrainian_Ukraine.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
 -- ----------------------------
 -- Table structure for drivers
 -- ----------------------------
-DROP TABLE IF EXISTS `drivers`;
-CREATE TABLE `drivers`  (
-                            `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT,
-                            `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                            `license_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                            `is_deleted` bit(1) NOT NULL DEFAULT b'0',
-                            PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE IF NOT EXISTS public.drivers
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    name character varying(45) COLLATE pg_catalog."default" NOT NULL,
+    license_number character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    login character varying(45) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    is_deleted boolean NOT NULL DEFAULT false,
+    CONSTRAINT drivers_pkey PRIMARY KEY (id)
+    );
 
 -- ----------------------------
 -- Table structure for manufacturers
 -- ----------------------------
-DROP TABLE IF EXISTS `manufacturers`;
-CREATE TABLE `manufacturers`  (
-                                  `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT,
-                                  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                                  `country` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                                  `is_deleted` bit(1) NOT NULL DEFAULT b'0',
-                                  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE IF NOT EXISTS public.manufacturers
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    name character varying(45) COLLATE pg_catalog."default" NOT NULL,
+    country character varying(45) COLLATE pg_catalog."default" NOT NULL,
+    is_deleted boolean NOT NULL DEFAULT false,
+    CONSTRAINT manufacturers_pkey PRIMARY KEY (id)
+    );
 
 -- ----------------------------
 -- Table structure for cars
 -- ----------------------------
-DROP TABLE IF EXISTS `cars`;
-CREATE TABLE `cars`  (
-                         `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT,
-                         `model` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                         `manufacturer_id` bigint(0) UNSIGNED NOT NULL,
-                         `is_deleted` bit(1) NOT NULL DEFAULT b'0',
-                         PRIMARY KEY (`id`) USING BTREE,
-                         INDEX `FK_manufacturer_id`(`manufacturer_id`) USING BTREE,
-                         CONSTRAINT `FK_manufacturer_id` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE IF NOT EXISTS public.cars
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    model character varying(45) COLLATE pg_catalog."default" NOT NULL,
+    manufacturer_id bigint NOT NULL,
+    is_deleted boolean NOT NULL DEFAULT false,
+    CONSTRAINT cars_pkey PRIMARY KEY (id),
+    CONSTRAINT cars_manufacturer_id_fk FOREIGN KEY (manufacturer_id)
+    REFERENCES public.manufacturers (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    );
 
 -- ----------------------------
 -- Table structure for cars_drivers
 -- ----------------------------
-DROP TABLE IF EXISTS `cars_drivers`;
-CREATE TABLE `cars_drivers`  (
-                                 `car_id` bigint(0) UNSIGNED NOT NULL,
-                                 `driver_id` bigint(0) UNSIGNED NOT NULL,
-                                 PRIMARY KEY (`car_id`, `driver_id`) USING BTREE,
-                                 INDEX `driver_id`(`driver_id`) USING BTREE,
-                                 INDEX `car_id`(`car_id`) USING BTREE,
-                                 CONSTRAINT `car_id` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-                                 CONSTRAINT `driver_id` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
+CREATE TABLE IF NOT EXISTS public.cars_drivers
+(
+    driver_id bigint NOT NULL,
+    car_id bigint NOT NULL,
+    CONSTRAINT cars_drivers_pk PRIMARY KEY (driver_id, car_id),
+    CONSTRAINT car_drivers_car_id_fk FOREIGN KEY (car_id)
+    REFERENCES public.cars (id) MATCH SIMPLE
+    ON UPDATE RESTRICT
+    ON DELETE RESTRICT,
+    CONSTRAINT car_drivers_driver_id_fk FOREIGN KEY (driver_id)
+    REFERENCES public.drivers (id) MATCH SIMPLE
+    ON UPDATE RESTRICT
+    ON DELETE RESTRICT
+    );
 SET FOREIGN_KEY_CHECKS = 1;
