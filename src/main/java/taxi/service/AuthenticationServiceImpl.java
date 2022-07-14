6dda5a990609fa112dd50c0b6769c7c949636b35
Maksym Karamavrov <makcumkaka@gmail.com>
2022-07-14 11:11:56 +0300

    Third commit. Exception throwing was refactored.

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -15,12 +15,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public Driver login(String username, String password) throws AuthenticationException {
         Optional<Driver> driver = driverDao.findByDriverLogin(username);
-        if (driver.isEmpty()) {
+        if (driver.isEmpty() || !driver.get().getPassword().equals(password)) {
             throw new AuthenticationException("Username or password was incorrect");
         }
-        if (driver.get().getPassword().equals(password)) {
-            return driver.get();
-        }
-        throw new AuthenticationException("Username or password was incorrect");
+        return driver.get();
     }
 }
