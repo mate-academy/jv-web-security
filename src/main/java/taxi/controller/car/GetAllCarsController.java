4de6c24ce1741d3ab91c1332eac2019ceaf19df7
Misha Polunin <polunin1986@gmail.com>
2022-07-18 13:03:37 +0300

    - done

--- src/main/java/taxi/controller/car/GetAllCarsController.java
+++ src/main/java/taxi/controller/car/GetAllCarsController.java
@@ -6,7 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import taxi.lib.Injector;
 import taxi.model.Car;
 import taxi.service.CarService;
@@ -15,12 +14,11 @@ public class GetAllCarsController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
     private final CarService carService = (CarService) injector.getInstance(CarService.class);
 
+    @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        Long driverId = (Long) session.getAttribute("driver_id");
-        List<Car> cars = carService.getAllByDriver(driverId);
-        req.setAttribute("cars", cars);
+        List<Car> allCars = carService.getAll();
+        req.setAttribute("cars", allCars);
         req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
     }
 }
