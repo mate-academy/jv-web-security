1be6dc68ea0ef55e1b874a66de14dee0feac99a8
Oleksii Mospan <alexey.mospan@gmail.com>
2022-08-01 20:14:58 +0300

    Added filters

---
+++ src/main/java/taxi/web/filter/LoggingFilter.java
@@ -0,0 +1,16 @@
+package taxi.web.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class LoggingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        chain.doFilter(request, response);
+    }
+}
