623158c8bcdab0685347a9152f2cfc6443d387ce
drapail <mv.dibrov@gmail.com>
2022-08-01 20:21:57 +0300

    renamed constants, removed empty lines and unused variables

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -3,20 +3,16 @@ package taxi.service;
 import java.util.Optional;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Inject;
-import taxi.lib.Injector;
 import taxi.lib.Service;
 import taxi.model.Driver;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
-    private static final Injector injector = Injector.getInstance("taxi");
     @Inject
     private DriverService driverService;
 
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
-        System.out.println("AuthenticationServiceImpl.login() was called with params: "
-                + "login= " + login + ", password= " + password);
         Optional<Driver> driver = driverService.findByLogin(login);
         if (driver.isPresent()
                 && driver.get().getPassword().equals(password)) {
