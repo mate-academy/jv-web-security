9922ba6c2a07019ea2605ddf9364ab84462bf908
Oleksii Mospan <alexey.mospan@gmail.com>
2022-08-02 14:29:20 +0300

    Changed allowed urls array to static.

--- src/main/java/taxi/web/filter/AuthenticationFilter.java
+++ src/main/java/taxi/web/filter/AuthenticationFilter.java
@@ -14,12 +14,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class AuthenticationFilter implements Filter {
-    private final Set<String> allowedUrls = new HashSet<>();
+    private static final Set<String> ALLOWED_URLS = new HashSet<>();
 
     @Override
     public void init(FilterConfig filterConfig) {
-        allowedUrls.add("/login");
-        allowedUrls.add("/drivers/add");
+        ALLOWED_URLS.add("/login");
+        ALLOWED_URLS.add("/drivers/add");
     }
 
     @Override
@@ -30,7 +30,7 @@ public class AuthenticationFilter implements Filter {
         HttpSession session = req.getSession();
         Long driverId = (Long) session.getAttribute("driver_id");
         System.out.println(req.getServletPath());
-        if (driverId != null || allowedUrls.contains(req.getServletPath())) {
+        if (driverId != null || ALLOWED_URLS.contains(req.getServletPath())) {
             chain.doFilter(req, resp);
             return;
         }
