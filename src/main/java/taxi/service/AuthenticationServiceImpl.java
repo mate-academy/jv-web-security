ef9622d1c9806f8d8f64c63b2b69daa3be2ba809
Oleksii Akishev <akishev.oleksii@gmail.com>
2022-08-04 17:34:13 +0300

    Method findByLogin was added to AuthSerImpl

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -1,10 +1,17 @@
 package taxi.service;
 
+import taxi.dao.DriverDao;
+import taxi.lib.Inject;
 import taxi.model.Driver;
 
+import java.util.Optional;
+
 public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private DriverDao driverDao;
     @Override
     public Driver login(String login, String password) {
+        Optional<Driver> driver = driverDao.findByLogin(login);
         return null;
     }
 }
