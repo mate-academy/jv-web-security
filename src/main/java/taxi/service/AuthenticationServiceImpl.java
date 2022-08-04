a1ae160aca92d3a9a053884a6958e47e8b4d7d55
Oleksii Akishev <akishev.oleksii@gmail.com>
2022-08-04 16:44:28 +0300

    AuthService was created

---
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -0,0 +1,10 @@
+package taxi.service;
+
+import taxi.model.Driver;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Override
+    public Driver login(String login, String password) {
+        return null;
+    }
+}
