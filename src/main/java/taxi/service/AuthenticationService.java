ac25e78d94438e6c037d29e6f08e6a3d3094d37d
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-08-17 18:14:30 +0200

    Bugs-fixed-and-build-success

--- src/main/java/taxi/service/AuthenticationService.java
+++ src/main/java/taxi/service/AuthenticationService.java
@@ -1,9 +1,8 @@
 package taxi.service;
 
+import java.util.Optional;
 import taxi.exception.AuthenticationException;
 import taxi.model.Driver;
-
-import java.util.Optional;
 
 public interface AuthenticationService {
     Optional<Driver> login(String login, String password) throws AuthenticationException;
--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -1,11 +1,10 @@
 package taxi.service;
 
+import java.util.Optional;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Injector;
 import taxi.lib.Service;
 import taxi.model.Driver;
-
-import java.util.Optional;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
