bde095527b791aaf4f3e041ce249360b25451b17
Max Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-08-18 02:46:37 +0300

    Set new attribute "servletPath" in IndexController

--- src/main/java/taxi/controller/IndexController.java
+++ src/main/java/taxi/controller/IndexController.java
@@ -10,6 +10,7 @@ public class IndexController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        req.setAttribute("servletPath", req.getServletPath());
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
