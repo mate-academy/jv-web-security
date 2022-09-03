e7d53c1e88d8adb0c6aa89e0dcfa391282720189
Maksym Bakunov <mpbakunov.it@gmail.com>
2022-09-03 23:45:32 +0200

    Implementation of task conditions

---
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -0,0 +1,27 @@
+package taxi.service;
+
+import java.util.Optional;
+import taxi.exception.AuthenticationException;
+import taxi.lib.Inject;
+import taxi.lib.Service;
+import taxi.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final String AUTHENTICATION_EXCEPTION_MESSAGE =
+            "Username or password is incorrect";
+    @Inject
+    private DriverService driverService;
+
+    @Override
+    public Driver login(String login, String password) throws AuthenticationException {
+        Optional<Driver> driver = driverService.findByLogin(login);
+        if (driver.isEmpty()) {
+            throw new AuthenticationException(AUTHENTICATION_EXCEPTION_MESSAGE);
+        }
+        if (driver.get().getPassword().equals(password)) {
+            return driver.get();
+        }
+        throw new AuthenticationException("AUTHENTICATION_EXCEPTION_MESSAGE");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/header.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<div>
+<a href="${pageContext.request.contextPath}/logout">
+    <button>
+        Logout
+    </button>
+</a>
+</div>
