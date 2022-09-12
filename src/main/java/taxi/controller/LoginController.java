0e318e04f664924708a05f35c83e0eb34d201d1d
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-09-12 16:05:11 +0300

    Authentication filter is implemented

--- src/main/java/taxi/controller/LoginController.java
+++ src/main/java/taxi/controller/LoginController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Injector;
+import taxi.model.Driver;
 import taxi.service.AuthenticationService;
 
 @WebServlet(urlPatterns = "/login")
@@ -33,7 +34,8 @@ public class LoginController extends HttpServlet {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
         try {
-            authenticationService.login(login, password);
+            Driver driver = authenticationService.login(login, password);
+            req.getSession().setAttribute("driver_id", driver.getId());
             resp.sendRedirect("/index");
         } catch (AuthenticationException e) {
             req.setAttribute("errorMessage", e.getMessage());
