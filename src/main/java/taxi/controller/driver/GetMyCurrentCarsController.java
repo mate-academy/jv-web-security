aff0a1a9fcc878358cf7801ec9f29ddb8edc5e0f
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-09-12 17:40:35 +0300

    Homework is done

---
+++ src/main/java/taxi/controller/driver/GetMyCurrentCarsController.java
@@ -0,0 +1,31 @@
+package taxi.controller.driver;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import taxi.lib.Injector;
+import taxi.model.Car;
+import taxi.service.CarService;
+import taxi.service.DriverService;
+
+@WebServlet(urlPatterns = "/drivers/my_current_cars")
+public class GetMyCurrentCarsController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final CarService carService = (CarService) injector.getInstance(CarService.class);
+    private final DriverService driverService =
+            (DriverService) injector.getInstance(DriverService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Long currentDriverId = (Long) req.getSession().getAttribute("driver_id");
+        List<Car> currentCarsByTheDriver = carService.getAllByDriver(currentDriverId);
+        req.setAttribute("driver_text", " my current");
+        req.setAttribute("cars", currentCarsByTheDriver);
+        req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
+    }
+}
