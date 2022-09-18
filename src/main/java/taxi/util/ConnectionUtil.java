ddc6474ec0510ba07302668742fd5ae0735292b9
Sergey Tsymbal <sfisher@ukr.net>
2022-09-19 00:40:15 +0300

    hw jv web security

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "YOUR DATABASE URL";
-    private static final String USERNAME = "YOUR USERNAME";
-    private static final String PASSWORD = "YOUR PASSWORD";
+    private static final String URL = "jdbc:mysql://localhost:3306/taxi";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
     private static final String JDBC_DRIVER = "YOUR DRIVER";
 
     static {
