dbaf0d6e3c4baa85c9e9be49e99efb9fda14f2dd
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-09-20 21:21:46 +0300

    method findByLogin was added in DriverDao and DriverDaoImpl

--- src/main/java/taxi/dao/DriverDaoImpl.java
+++ src/main/java/taxi/dao/DriverDaoImpl.java
@@ -56,6 +56,23 @@ public class DriverDaoImpl implements DriverDao {
     }
 
     @Override
+    public Optional<Driver> findByLogin(String login) {
+        String query = "SELECT * FROM drivers WHERE login = ? AND is_deleted = FALSE";
+        try (Connection connection = ConnectionUtil.getConnection();
+               PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, login);
+            ResultSet resultSet = statement.executeQuery();
+            Driver driver = null;
+            if (resultSet.next()) {
+                driver = parseDriverFromResultSet(resultSet);
+            }
+            return Optional.ofNullable(driver);
+        } catch (SQLException e) {
+            throw new DataProcessingException("Couldn't get driver by login "  + login,  e);
+        }
+    }
+
+    @Override
     public List<Driver> getAll() {
         String query = "SELECT * FROM drivers WHERE is_deleted = FALSE";
         List<Driver> drivers = new ArrayList<>();
