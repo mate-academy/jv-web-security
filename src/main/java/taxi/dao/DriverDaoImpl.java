a999280434d79eac30bdc17507f1a81a76ffb455
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-09-20 23:22:43 +0300

    LoginController and its corresponding jsp was added

--- src/main/java/taxi/dao/DriverDaoImpl.java
+++ src/main/java/taxi/dao/DriverDaoImpl.java
@@ -59,7 +59,7 @@ public class DriverDaoImpl implements DriverDao {
     public Optional<Driver> findByLogin(String login) {
         String query = "SELECT * FROM drivers WHERE login = ? AND is_deleted = FALSE";
         try (Connection connection = ConnectionUtil.getConnection();
-               PreparedStatement statement = connection.prepareStatement(query)) {
+                PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, login);
             ResultSet resultSet = statement.executeQuery();
             Driver driver = null;
@@ -68,7 +68,7 @@ public class DriverDaoImpl implements DriverDao {
             }
             return Optional.ofNullable(driver);
         } catch (SQLException e) {
-            throw new DataProcessingException("Couldn't get driver by login "  + login,  e);
+            throw new DataProcessingException("Couldn't get driver by login " + login, e);
         }
     }
 
---
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -0,0 +1,12 @@
+package taxi.service;
+
+import taxi.lib.Service;
+import taxi.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Override
+    public Driver login(String login, String password) {
+        return null;
+    }
+}
