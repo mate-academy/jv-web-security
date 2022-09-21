a42b0fb62c6d37d6349d13bc28b4e54c6f2a59c1
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-09-21 19:12:18 +0300

    Set of allowed url was added to AuthenticationFilter

--- src/main/java/taxi/web/filter/AuthenticationFilter.java
+++ src/main/java/taxi/web/filter/AuthenticationFilter.java
@@ -1,8 +1,11 @@
 package taxi.web.filter;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -11,6 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class AuthenticationFilter implements Filter {
+    private Set<String> allowedUrls = new HashSet<>();
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        allowedUrls.add("/drivers/add");
+        allowedUrls.add("/login");
+    }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
@@ -18,8 +29,7 @@ public class AuthenticationFilter implements Filter {
         HttpServletResponse resp = (HttpServletResponse) response;
         HttpSession session = req.getSession();
         Long driverId = (Long) session.getAttribute("driverId");
-        if (driverId == null && (req.getServletPath().equals("/drivers/add")
-                || req.getServletPath().equals("/login"))) {
+        if (driverId == null && allowedUrls.contains(req.getServletPath())) {
             chain.doFilter(req, resp);
             return;
         }
