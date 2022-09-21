8cd9ba34289510a1cd16a1c0b6f96b4468182fb6
GolDenik <denik.golibroda@gmail.com>
2022-09-21 18:00:16 +0300

    fixed some mistakes

--- src/main/java/taxi/filter/AuthenticationFilter.java
+++ src/main/java/taxi/filter/AuthenticationFilter.java
@@ -19,7 +19,7 @@ public class AuthenticationFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) {
         allowedUrls.add("/login");
-        allowedUrls.add("/drivers/register");
+        allowedUrls.add("/register");
     }
 
     @Override
