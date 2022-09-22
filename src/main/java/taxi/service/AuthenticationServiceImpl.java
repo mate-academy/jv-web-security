a8025e8b14c0b105a8789b2b78368bf7e4efd69e
ArturZhadan <artur.zhadan.17@gmail.com>
2022-09-22 11:56:33 +0300

    changed AuthenticationServiceImpl login() method

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -14,11 +14,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
         Optional<Driver> driver = driverService.findByLogin(login);
-        if (driver.isEmpty()) {
-            throw new AuthenticationException("Login or password was incorrect");
-        }
-        if (driver.get().getPassword().equals(password)) {
-            return driver.get();
+        if (driver.isPresent()) {
+            if (driver.get().getPassword().equals(password)) {
+                return driver.get();
+            }
         }
         throw new AuthenticationException("Login or password was incorrect");
     }
