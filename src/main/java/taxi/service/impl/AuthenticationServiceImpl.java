a5d47cc5b24e758e4fda392f8aef74cb052be236
Veers <raadch3011@gmail.com>
2022-09-22 14:46:30 +0300

    create GetMyCurrentCarsController class

--- src/main/java/taxi/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/impl/AuthenticationServiceImpl.java
@@ -1,10 +1,12 @@
-package taxi.service;
+package taxi.service.impl;
 
 import java.util.Optional;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Inject;
 import taxi.lib.Service;
 import taxi.model.Driver;
+import taxi.service.AuthenticationService;
+import taxi.service.DriverService;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
