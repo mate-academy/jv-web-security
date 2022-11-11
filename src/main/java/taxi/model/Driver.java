3a815001d34b906dc0c4784c9cf19b4b09dcbada
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-11-11 12:42:49 +0200

    Added correct data to ConnectionUtil Added columns `login` and `password` to `taxi.drivers` table Changed drivers.add.jsp and drivers.all.jsp CarDaoImpl and DriverDaoImpl methods were changed with correct columns

--- src/main/java/taxi/model/Driver.java
+++ src/main/java/taxi/model/Driver.java
@@ -6,6 +6,8 @@ public class Driver {
     private Long id;
     private String name;
     private String licenseNumber;
+    private String login;
+    private String password;
 
     public Driver() {
     }
@@ -13,6 +15,17 @@ public class Driver {
     public Driver(String name, String licenseNumber) {
         this.name = name;
         this.licenseNumber = licenseNumber;
+    }
+
+    public Driver(String name, String licenseNumber, String login, String password) {
+        this(name, licenseNumber);
+        this.login = login;
+        this.password = password;
+    }
+
+    public Driver(Long id, String name, String licenseNumber, String login, String password) {
+        this(name, licenseNumber, login, password);
+        this.id = id;
     }
 
     public Long getId() {
@@ -39,6 +52,22 @@ public class Driver {
         this.licenseNumber = licenseNumber;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -48,13 +77,14 @@ public class Driver {
             return false;
         }
         Driver driver = (Driver) o;
-        return Objects.equals(id, driver.id)
-                && Objects.equals(name, driver.name)
-                && Objects.equals(licenseNumber, driver.licenseNumber);
+        return id.equals(driver.id) && name.equals(driver.name)
+                && licenseNumber.equals(driver.licenseNumber)
+                && login.equals(driver.login)
+                && password.equals(driver.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, licenseNumber);
+        return Objects.hash(id, name, licenseNumber, login, password);
     }
 }
