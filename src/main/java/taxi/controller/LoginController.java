f6aa66f93bc51964ee4722f6490d0b3bd3f65d04
AntonPav <streetreadger@gmail.com>
2022-11-15 00:21:21 +0200

    Implement logging and allow only add driver and login for non authentificated user

--- src/main/java/taxi/controller/LoginController.java
+++ src/main/java/taxi/controller/LoginController.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/login")
@@ -33,9 +34,11 @@ public class LoginController extends HttpServlet {
             throws ServletException, IOException {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
-
+        HttpSession session = req.getSession();
         try {
             Driver loggedDriver = authenticationService.login(login, password);
+            session.setAttribute("user_id", loggedDriver.getId());
+            req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
         } catch (AuthenticationException e) {
             req.setAttribute("message", e.getMessage());
             req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
