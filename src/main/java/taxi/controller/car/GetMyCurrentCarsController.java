0e8c538bb7c61d308e0d1015b6267eb329bf5040
Yura Hryniv <superyura444@gmail.com>
2022-11-15 13:18:01 +0200

    Login, logout and filter are implemented

---
+++ src/main/java/taxi/controller/car/GetMyCurrentCarsController.java
@@ -0,0 +1,30 @@
+package taxi.controller.car;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import taxi.lib.Injector;
+import taxi.model.Car;
+import taxi.service.CarService;
+
+public class GetMyCurrentCarsController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final CarService carService = (CarService) injector.getInstance(CarService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        HttpSession session = req.getSession();
+        Long driverId = (Long) session.getAttribute("driver_id");
+
+        List<Car> allCarsPerDriver = carService.getAllByDriver(driverId);
+        req.setAttribute("cars", allCarsPerDriver);
+        req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
+    }
+}
