1a70bba745cfc35641f825f634f86a03708cd3e2
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2022-11-18 06:54:03 +0200

    added fixes after review

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -1,16 +1,19 @@
 package taxi.service;
 
 import java.util.Optional;
-import taxi.dao.DriverDao;
 import taxi.exception.AuthenticationException;
+import taxi.lib.Inject;
+import taxi.lib.Service;
 import taxi.model.Driver;
 
+@Service
 public class AuthenticationServiceImpl implements AuthenticationService {
-    private DriverDao driverDao;
+    @Inject
+    private DriverService driverService;
 
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
-        Optional<Driver> driver = driverDao.findByLogin(login);
+        Optional<Driver> driver = driverService.findByLogin(login);
         if (driver.isEmpty() || !driver.get().getPassword().equals(password)) {
             throw new AuthenticationException("Driver login or password was incorrect");
         }
