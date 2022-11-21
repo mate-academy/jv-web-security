a65beee389fa292e8ac293ff2bb5370ae8240ec7
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-11-21 21:05:32 +0200

    little fix

--- src/main/java/taxi/dao/DriverDaoImpl.java
+++ src/main/java/taxi/dao/DriverDaoImpl.java
@@ -105,10 +105,19 @@ public class DriverDaoImpl implements DriverDao {
 
     @Override
     public Optional<Driver> findByLogin(String login) {
-        return getAll()
-                .stream()
-                .filter(f -> f.getLogin().equals(login))
-                .findFirst();
+        String query = "SELECT * FROM drivers WHERE login = ? AND is_deleted = FALSE;";
+        try (Connection connection = ConnectionUtil.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, login);
+            ResultSet resultSet = statement.executeQuery();
+            Driver driver = null;
+            if (resultSet.next()) {
+                driver = parseDriverFromResultSet(resultSet);
+            }
+            return Optional.of(driver);
+        } catch (SQLException e) {
+            throw new DataProcessingException("Couldn't get driver by login " + login, e);
+        }
     }
 
     private Driver parseDriverFromResultSet(ResultSet resultSet) throws SQLException {
