fa457be78211ad44dfebad213802cc1da7800cf5
Alex Anofriev <alex.linkoln7391@gmail.com>
2022-11-23 18:19:59 +0200

    login implementation

--- src/main/java/taxi/controller/DriverIndexController.java
+++ src/main/java/taxi/controller/DriverIndexController.java
@@ -2,13 +2,12 @@ package taxi.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 public class DriverIndexController extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
