e14229fc1e52382eb8aafef4108bbc9f165d7c7a
MuzykaVP <muzykavp.work@gmail.com>
2022-11-25 11:59:18 +0200

    AuthenticationService was implemented, CarDao and CarService was modified, GetMyCurrentCars, Login and Logout controllers was implemented

---
+++ src/main/java/taxi/controller/LogoutController.java
@@ -0,0 +1,28 @@
+package taxi.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
+
+@WebServlet(urlPatterns = "/logout")
+public class LogoutController extends HttpServlet {
+    private static final Injector injector =
+            Injector.getInstance("taxi");
+    private final DriverService driverService =
+            (DriverService) injector.getInstance(DriverService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Long driverId = (Long) req.getSession().getAttribute("driver_id");
+        Driver driver = driverService.get(driverId);
+        req.setAttribute("login", driver.getLogin());
+        req.getSession().invalidate();
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/header.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h4>${login}</h4>
+<a href="${pageContext.request.contextPath}/logout">Logout</a>
+</body>
+</html>
