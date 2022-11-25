b5e1c95a6424d6add60b7c0983957e356c4a972f
Bodddd <skrypnikbod@gmail.com>
2022-11-25 13:30:15 +0200

    Create new AuthenticationService with method

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -1,10 +1,26 @@
 package taxi.service;
 
+import taxi.exception.AuthenticationException;
+import taxi.lib.Inject;
+import taxi.lib.Service;
 import taxi.model.Driver;
 
+import java.util.Optional;
+
+@Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    DriverService driverService;
+
     @Override
-    public Driver login(String login, String password) {
-        return null;
+    public Driver login(String login, String password) throws AuthenticationException {
+        Optional<Driver> driver = driverService.findByLogin(login);
+        if (driver.isEmpty()) {
+            throw new AuthenticationException("Login or password is incorrect");
+        }
+        if (driver.get().getPassword().equals(password)) {
+            return driver.get();
+        }
+        throw new AuthenticationException("Login or password is incorrect");
     }
 }
