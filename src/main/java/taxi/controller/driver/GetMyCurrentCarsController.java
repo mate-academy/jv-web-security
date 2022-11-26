3175815a4c27560ceaa2db6f51e0d5ed94de6df4
Andrii Matveev <matveev.tesla@gmail.com>
2022-11-26 15:49:29 +0200

    code has been improved

--- src/main/java/taxi/controller/driver/GetMyCurrentCarsController.java
+++ src/main/java/taxi/controller/driver/GetMyCurrentCarsController.java
@@ -9,16 +9,17 @@ import javax.servlet.http.HttpServletResponse;
 import taxi.dao.CarDao;
 import taxi.lib.Injector;
 import taxi.model.Car;
+import taxi.service.CarService;
 
 public class GetMyCurrentCarsController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
-    private final CarDao carDao = (CarDao) injector.getInstance(CarDao.class);
+    private final CarService carService = (CarService) injector.getInstance(CarService.class);
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         Long driverId = (Long) req.getSession().getAttribute("driver_id");
-        List<Car> allCars = carDao.getAllByDriver(driverId);
+        List<Car> allCars = carService.getAllByDriver(driverId);
         req.setAttribute("cars", allCars);
         req.getRequestDispatcher("/WEB-INF/views/drivers/cars/all.jsp").forward(req, resp);
     }
