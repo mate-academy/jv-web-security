8897fea0a762e5a7cf938ec3cca90e2c104dde85
Oleg Мarchuk <mar4uk14@gmail.com>
2022-11-28 12:04:08 +0200

    Fix some mistakes.

--- src/main/java/taxi/controller/car/GetMyCurrentCarsController.java
+++ src/main/java/taxi/controller/car/GetMyCurrentCarsController.java
@@ -21,8 +21,8 @@ public class GetMyCurrentCarsController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         Long driverId = (Long) req.getSession().getAttribute("driver_id");
-        List<Car> allCars = carService.getAllByDriver(driverId);
-        req.setAttribute("cars",allCars);
+        List<Car> cars = carService.getAllByDriver(driverId);
+        req.setAttribute("cars",cars);
         req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req,resp);
     }
 }
