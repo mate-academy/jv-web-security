d92e7d41d0b90ca80c19cb8861324a9314a3f51c
Denys Domashevskyi <denis.domashevskiy28@gmail.com>
2022-11-29 11:28:47 +0200

    Remove unused imports

--- src/main/java/taxi/controller/car/GetMyCurrentCarsController.java
+++ src/main/java/taxi/controller/car/GetMyCurrentCarsController.java
@@ -1,13 +1,11 @@
 package taxi.controller.car;
 
 import java.io.IOException;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import taxi.lib.Injector;
-import taxi.model.Car;
 import taxi.service.CarService;
 
 public class GetMyCurrentCarsController extends HttpServlet {
