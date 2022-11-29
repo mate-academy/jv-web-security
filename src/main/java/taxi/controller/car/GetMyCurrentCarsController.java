2793ef7fe69aa772b09208d1ce357aba3f78df67
Bodddd <skrypnikbod@gmail.com>
2022-11-29 04:24:04 +0200

    combine throw, add config to web.xml, add 1 empty line

--- src/main/java/taxi/controller/car/GetMyCurrentCarsController.java
+++ src/main/java/taxi/controller/car/GetMyCurrentCarsController.java
@@ -2,7 +2,6 @@ package taxi.controller.car;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpSession;
 import taxi.lib.Injector;
 import taxi.service.CarService;
 
-@WebServlet(urlPatterns = "/drivers/cars")
 public class GetMyCurrentCarsController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
     private final CarService carService = (CarService) injector.getInstance(CarService.class);
