8b45f81d2ca55fd13a6e85e70eb99fd3988d2c3e
Dima Davydenko <dmytro.davydenko89@gmail.com>
2022-12-13 21:24:35 +0200

    procfile and heroku plugin were added

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,9 +6,11 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi_service";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "1289456";
+    private static final String URL = "mysql://kp6nwg7gfi45xc8n"
+            + ":p7ys0m2lpfe2tg6a@clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com"
+            + ":3306/vqjb8k4o4gba8kwo";
+    private static final String USERNAME = "kp6nwg7gfi45xc8n";
+    private static final String PASSWORD = "p7ys0m2lpfe2tg6a";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
