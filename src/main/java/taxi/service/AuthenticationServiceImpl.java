2075be4cb623ceeeae3a75801acf619643f83462
AndriySichniy <sichniy.andriy@gmail.com>
2022-12-21 14:30:58 +0200

    logic in authService

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -8,8 +8,10 @@ import taxi.model.Driver;
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
     private DriverService driverService;
+
     @Override
     public Driver login(String login, String password) {
-        return driverService.findByLogin(login).orElse(null);
+        Driver driver = driverService.findByLogin(login).orElse(null);
+        return driver != null && driver.getPassword().equals(password) ? driver : null;
     }
 }
