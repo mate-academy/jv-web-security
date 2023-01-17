504fa63b762ae0341766ec751e6761e75b3e14a9
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-01-17 17:17:58 +0100

    solution for web-security

--- src/main/java/taxi/model/Driver.java
+++ src/main/java/taxi/model/Driver.java
@@ -6,13 +6,17 @@ public class Driver {
     private Long id;
     private String name;
     private String licenseNumber;
+    private String login;
+    private String password;
 
     public Driver() {
     }
 
-    public Driver(String name, String licenseNumber) {
+    public Driver(String name, String licenseNumber, String login, String password) {
         this.name = name;
         this.licenseNumber = licenseNumber;
+        this.login = login;
+        this.password = password;
     }
 
     public Long getId() {
@@ -26,7 +30,13 @@ public class Driver {
     public String getName() {
         return name;
     }
+    public String getLogin()  {
+        return login;
+    }
 
+    public String getPassword() {
+        return password;
+    }
     public void setName(String name) {
         this.name = name;
     }
@@ -39,22 +49,28 @@ public class Driver {
         this.licenseNumber = licenseNumber;
     }
 
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Driver driver = (Driver) o;
         return Objects.equals(id, driver.id)
                 && Objects.equals(name, driver.name)
-                && Objects.equals(licenseNumber, driver.licenseNumber);
+                && Objects.equals(licenseNumber, driver.licenseNumber)
+                && Objects.equals(login, driver.login)
+                && Objects.equals(password, driver.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, licenseNumber);
+        return Objects.hash(id, name, licenseNumber, login, password);
     }
 }
