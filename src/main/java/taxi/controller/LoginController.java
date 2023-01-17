355d1ebf396d8ff14d02e625b9acf9ac3cabdb21
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-01-17 17:35:50 +0100

    solution for web-security.style changes

--- src/main/java/taxi/controller/LoginController.java
+++ src/main/java/taxi/controller/LoginController.java
@@ -6,9 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import taxi.exception.AuthenticationException;
 import taxi.lib.Injector;
 import taxi.model.Driver;
-import taxi.exception.AuthenticationException;
 import taxi.service.AuthenticationService;
 
 public class LoginController extends HttpServlet {
