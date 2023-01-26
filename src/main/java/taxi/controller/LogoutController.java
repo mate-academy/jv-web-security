e8a5ecbac940ec80b057f74d425279d49d4929c9
Uka0001 <ukraine0001@gmail.com>
2023-01-27 00:58:13 +0200

    logout implemented

--- src/main/java/taxi/controller/LogoutController.java
+++ src/main/java/taxi/controller/LogoutController.java
@@ -1,6 +1,7 @@
 package taxi.controller;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        super.doGet(req, resp);
+        req.getSession().invalidate();
+        resp.sendRedirect("/login.jsp");
     }
 }
