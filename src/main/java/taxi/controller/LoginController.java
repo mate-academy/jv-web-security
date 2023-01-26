ce5b19dffa20b3ec25797c0e33f3d6a9fa533c7d
Uka0001 <ukraine0001@gmail.com>
2023-01-26 20:03:26 +0200

    login.jsp page and login controller added

--- src/main/java/taxi/controller/LoginController.java
+++ src/main/java/taxi/controller/LoginController.java
@@ -1,4 +1,27 @@
 package taxi.controller;
 
-public class LoginController {
+import taxi.model.Driver;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginController extends HttpServlet {
+    private AutentificationService autentificationService;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+    }
+
+    Driver driver = autentificationService.login(login, password);
 }
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yulia
+  Date: 26.01.2023
+  Time: 19:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login page</title>
+</head>
+<body>
+<h1>Login page</h1>
+<form method="post" action="${pageContext.request.contextPath}/login">
+Please enter your login <input type="text" name="login" required>
+Please enter your password <input type="password" name="password" required>
+<button type="submit">Login</button>
+</form>
+<h4><a href="{pageContext.request.contextPath}/register">Register</a></h4>
+</body>
+</html>
