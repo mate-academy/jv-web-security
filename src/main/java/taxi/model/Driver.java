62e68a3b2fb65f724409605c5b338f7eedba89eb
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-01-29 01:46:52 +0000

    Add two more fields (login and password) to Driver class.

--- src/main/java/taxi/model/Driver.java
+++ src/main/java/taxi/model/Driver.java
@@ -7,12 +7,17 @@ public class Driver {
     private String name;
     private String licenseNumber;
 
+    private String login;
+    private String password;
+
     public Driver() {
     }
 
-    public Driver(String name, String licenseNumber) {
+    public Driver(String name, String licenseNumber, String login, String password) {
         this.name = name;
         this.licenseNumber = licenseNumber;
+        this.login = login;
+        this.password = password;
     }
 
     public Long getId() {
@@ -39,6 +44,22 @@ public class Driver {
         this.licenseNumber = licenseNumber;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -57,4 +78,5 @@ public class Driver {
     public int hashCode() {
         return Objects.hash(id, name, licenseNumber);
     }
+
 }
