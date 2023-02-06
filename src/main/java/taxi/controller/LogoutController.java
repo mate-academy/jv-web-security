c4f091a09e733c08fe2c2e650632314e73dcba1c
ydikalo <dikalotel@gmail.com>
2023-02-06 21:18:23 +0200

    removed redundant exception from doGet method signature of LogoutController

--- src/main/java/taxi/controller/LogoutController.java
+++ src/main/java/taxi/controller/LogoutController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+            throws IOException {
         req.getSession().invalidate();
         resp.sendRedirect("/login.jsp");
     }
