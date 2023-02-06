ca980c6213e1f1865bc9db918c4c06ed8fd9c86c
Ihor Vynogradskyi <avassorv@gmail.com>
2023-02-06 18:08:31 +0200

    web security sec

--- src/main/java/taxi/controller/LogoutController.java
+++ src/main/java/taxi/controller/LogoutController.java
@@ -1,7 +1,6 @@
 package taxi.controller;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+            throws IOException {
         req.getSession().invalidate();
         resp.sendRedirect("/login");
     }
