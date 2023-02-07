67029c70ce7e63ef9967e076fdf38eb95a984e5e
Artem Voitiuk <artemon2504@gmail.com>
2023-02-07 12:11:03 +0200

    add authentication to project 1 commit

--- src/main/java/taxi/controller/authentication/LoginController.java
+++ src/main/java/taxi/controller/authentication/LoginController.java
@@ -1,45 +1,40 @@
-package taxi.controller.driver;
+package taxi.controller.authentication;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Injector;
 import taxi.model.Driver;
 import taxi.service.AuthenticationService;
-import taxi.service.DriverService;
 
-public class AddDriverController extends HttpServlet {
+public class LoginController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
-    private final DriverService driverService = (DriverService) injector
-            .getInstance(DriverService.class);
     private final AuthenticationService authenticationService = (AuthenticationService) injector
             .getInstance(AuthenticationService.class);
 
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
     }
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException, ServletException {
-        String name = req.getParameter("name");
-        String licenseNumber = req.getParameter("license_number");
+            throws ServletException, IOException {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
-        Driver driver = new Driver(name, licenseNumber, login, password);
         try {
-            authenticationService.checkIfLoginExists(login);
-            driverService.create(driver);
-            resp.sendRedirect(req.getContextPath() + "/drivers/add");
+            Driver driver = authenticationService.login(login, password);
+            HttpSession session = req.getSession();
+            session.setAttribute("driver_id", driver.getId());
+            resp.sendRedirect(req.getContextPath() + "/index");
         } catch (AuthenticationException e) {
             req.setAttribute("errorMsg", e.getMessage());
-            req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+            req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
         }
     }
 }
