9606ddb449203a02a8c066831b095384d7062cd8
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-02-07 16:10:36 +0100

    security. build success

--- src/main/java/taxi/controller/LoginController.java
+++ src/main/java/taxi/controller/LoginController.java
@@ -14,7 +14,7 @@ import taxi.service.AuthenticationService;
 public class LoginController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
 
-    private AuthenticationService authenticationService =
+    private final AuthenticationService authenticationService =
             (AuthenticationService) injector.getInstance(AuthenticationService.class);
 
     @Override
--- src/main/java/taxi/dao/DriverDaoImpl.java
+++ src/main/java/taxi/dao/DriverDaoImpl.java
@@ -130,7 +130,7 @@ public class DriverDaoImpl implements DriverDao {
             }
             return Optional.ofNullable(driver);
         } catch (SQLException e) {
-            throw new DataProcessingException("Couldn't find driver by login " + login, e);
+            throw new DataProcessingException("Can't find driver by login " + login, e);
         }
     }
 }
