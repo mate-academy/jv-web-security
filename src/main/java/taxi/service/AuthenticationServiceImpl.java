b2e1f198a46f130244de07ae2795f40fb88a915c
Sofiia <sofiakazzz@gmail.com>
2023-02-21 20:12:18 +0200

    Was created needed controller( blocks if was combined and added context path to all sendRedirect)

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -15,10 +15,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public Driver login(String driverName, String password) throws AuthenticationException {
         Optional<Driver> driver = driverDao.findByLogin(driverName);
-        if (driver.isEmpty()) {
-            throw new AuthenticationException("Login or password is incorrect");
-        }
-        if (driver.get().getPassword().equals(password)) {
+        if (driver.isPresent() && driver.get().getPassword().equals(password)) {
             return driver.get();
         }
         throw new AuthenticationException("Login or password is incorrect");
