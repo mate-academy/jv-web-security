779c04cba1d6146b88a303edaae2ccf86ceef438
Sofiia <sofiakazzz@gmail.com>
2023-02-21 19:48:55 +0200

    Was created needed controller

---
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -0,0 +1,26 @@
+package taxi.service;
+
+import java.util.Optional;
+import taxi.dao.DriverDao;
+import taxi.exeption.AuthenticationException;
+import taxi.lib.Inject;
+import taxi.lib.Service;
+import taxi.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private DriverDao driverDao;
+
+    @Override
+    public Driver login(String driverName, String password) throws AuthenticationException {
+        Optional<Driver> driver = driverDao.findByLogin(driverName);
+        if (driver.isEmpty()) {
+            throw new AuthenticationException("Login or password is incorrect");
+        }
+        if (driver.get().getPassword().equals(password)) {
+            return driver.get();
+        }
+        throw new AuthenticationException("Login or password is incorrect");
+    }
+}
