5f6d74f588a53b0f120cdb6f209e909321465975
Richard <richarddkochut@gmail.com>
2023-02-22 22:46:37 +0200

    done

---
+++ src/main/java/taxi/service/AuthenticationService.java
@@ -0,0 +1,10 @@
+package taxi.service;
+
+import taxi.exception.AuthenticationException;
+import taxi.model.Driver;
+
+public interface AuthenticationService {
+
+    Driver login(String username, String password) throws AuthenticationException;
+
+}
