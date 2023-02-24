fd78b09925b2b765974e217e88e5df6ad8dd2dbd
carbo <carbon7557@gmail.com>
2023-02-24 11:34:12 +0000

    Task is done.

--- src/main/java/taxi/controller/car/AddCarController.java
+++ src/main/java/taxi/controller/car/AddCarController.java
@@ -25,7 +25,7 @@ public class AddCarController extends HttpServlet {
 
     @Override
         public void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException, ServletException {
+                throws IOException, ServletException {
         String model = req.getParameter("model");
         long manufacturerId = Long.parseLong(req.getParameter("manufacturer_id"));
         Manufacturer manufacturer = manufacturerService.get(manufacturerId);
