6717470bf25d2ff66c1bd4d8d4fc10e9fc3b53ca
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-02-27 14:51:21 +0200

    feat: jv-web-security #comment was fixd issues after review

--- src/main/java/taxi/dao/DriverDaoImpl.java
+++ src/main/java/taxi/dao/DriverDaoImpl.java
@@ -104,7 +104,7 @@ public class DriverDaoImpl implements DriverDao {
 
     @Override
     public Optional<Driver> findByLogin(String login) {
-        String query = "SELECT * FROM drivers WHERE login = ?";
+        String query = "SELECT * FROM drivers WHERE login = ? AND is_deleted = FALSE";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, login);
