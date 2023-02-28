44c58a0e05ac8221281f0784e847a94102f6c572
Andrii Pylypenko <pilipenko1975@gmail.com>
2023-02-28 11:33:12 +0200

    jv-web-security v.1.0.

--- src/main/java/taxi/controller/LogoutController.java
+++ src/main/java/taxi/controller/LogoutController.java
@@ -1,17 +1,18 @@
 package taxi.controller;
 
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet(urlPatterns = "/index")
-public class IndexController extends HttpServlet {
+@WebServlet(urlPatterns = "/logout")
+public class LogoutController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+        req.getSession().invalidate();
+        resp.sendRedirect("/");
     }
 }
