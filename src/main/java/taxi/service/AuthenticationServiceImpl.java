fe7737ddb3b0900a6e8db663959276a10f517841
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-03-29 00:18:07 +0300

    refactoring

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -10,12 +10,12 @@ import taxi.model.Driver;
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
-    DriverDao driverDao;
+    DriverService driverService;
 
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
-        Optional<Driver> driver = driverDao.findByLogin(login);
-        if(driver.isPresent() && driver.get().getPassword().equals(password)) {
+        Optional<Driver> driver = driverService.findByLogin(login);
+        if (driver.isPresent() && driver.get().getPassword().equals(password)) {
             return driver.get();
         }
         throw new AuthenticationException("Login or password was incorrect");
