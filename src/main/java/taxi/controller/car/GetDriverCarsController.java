25244af3037e920361794c66888bc8e149100d98
Serhii Shyian <serhii.shiyan@ukr.net>
2023-03-28 19:14:09 +0300

    1) In exception package added AuthenticationException class 2) Created method Optional<Driver> findByLogin(String login) in DriverDao and DriverService 3) Created AuthenticationService with method Driver login(String login, String password) and findByLogin method 4) Implemented LoginController with corresponding JSP page 5) Implemented LogoutController and add a corresponding button on all pages where it should be 6) Implemented user authentication based on HttpSession and filters 7) Created GetDriverCarsController and added reference to index.jsp

---
+++ src/main/java/taxi/controller/car/GetDriverCarsController.java
@@ -0,0 +1,32 @@
+package taxi.controller.car;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import taxi.model.Car;
+import taxi.service.CarService;
+import taxi.service.DriverService;
+import taxi.util.Injector;
+
+public class GetDriverCarsController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final CarService carService = (CarService) injector.getInstance(CarService.class);
+    private final DriverService driverService =
+            (DriverService) injector.getInstance(DriverService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        req.setAttribute("driver_name", session.getAttribute("driver_name"));
+        Long driverId = Long.parseLong(String.valueOf(session.getAttribute("driver_id")));
+        List<Car> carsByDriver = carService.getAllByDriver(driverId);
+        req.setAttribute("cars", carsByDriver);
+        req.setAttribute("drivers", driverService.getAll());
+        req.getRequestDispatcher("/WEB-INF/views/car/all.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/java/taxi/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,22 @@
+package taxi.service.impl;
+
+import java.util.Optional;
+import taxi.exception.AuthenticationException;
+import taxi.lib.Inject;
+import taxi.model.Driver;
+import taxi.service.AuthenticationService;
+import taxi.service.DriverService;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private DriverService driverService;
+
+    @Override
+    public Driver login(String login, String password) throws AuthenticationException {
+        Optional<Driver> driver = driverService.findByLogin(login);
+        if (driver.isEmpty() || !driver.get().getPassword().equals(password)) {
+            throw new AuthenticationException("Login or password was incorrect");
+        }
+        return driver.get();
+    }
+}
