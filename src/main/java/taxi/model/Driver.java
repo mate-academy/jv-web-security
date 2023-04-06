d88498d08e7b2a39090eea9f5085a416079452bf
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-04-07 01:11:15 +0300

    Created new parameters (login, password) to column drivers Added new fields (login, password) for Driver's model Modified DriverDaoImpl to fetch all columns for drivers, including new ones Created new interface AuthenticationService Create new method Optional<Driver> findByLogin(String login) in DriverDao and DriverService

--- src/main/java/taxi/model/Driver.java
+++ src/main/java/taxi/model/Driver.java
@@ -6,6 +6,8 @@ public class Driver {
     private Long id;
     private String name;
     private String licenseNumber;
+    private String login;
+    private String password;
 
     public Driver() {
     }
@@ -13,6 +15,22 @@ public class Driver {
     public Driver(String name, String licenseNumber) {
         this.name = name;
         this.licenseNumber = licenseNumber;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Long getId() {
---
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -0,0 +1,17 @@
+package taxi.service;
+
+import taxi.dao.DriverDao;
+import taxi.lib.Inject;
+import taxi.lib.Service;
+import taxi.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private DriverDao driverDao;
+
+    @Override
+    public Driver login(String login, String password) {
+        return null;
+    }
+}
