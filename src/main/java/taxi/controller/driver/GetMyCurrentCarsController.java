7243d98fd480838655d8a632fd56552953ed9813
Yuriy Turchin <turchin.yuriy1@gmail.com>
2023-04-06 22:35:59 +0300

    add controller GetMyCurrentCar and fix import

--- src/main/java/taxi/controller/driver/GetMyCurrentCarsController.java
+++ src/main/java/taxi/controller/driver/GetMyCurrentCarsController.java
@@ -11,12 +11,12 @@ import taxi.lib.Injector;
 import taxi.model.Car;
 import taxi.service.CarService;
 
-
 @WebServlet(urlPatterns = "/cars/my")
 public class GetMyCurrentCarsController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
     private final CarService carService =
             (CarService) injector.getInstance(CarService.class);
+
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
