748fa5fce56cf67947e3c3f281336f653cf1e828
Nikita <nsalogub96@gmail.com>
2023-04-07 23:11:45 +0300

    implemets filter, login/logout/getcurrent car controller and make some changes in project

--- src/main/java/taxi/model/Driver.java
+++ src/main/java/taxi/model/Driver.java
@@ -6,6 +6,8 @@ public class Driver {
     private Long id;
     private String name;
     private String licenseNumber;
+    private String login;
+    private String password;
 
     public Driver() {
     }
@@ -13,6 +15,21 @@ public class Driver {
     public Driver(String name, String licenseNumber) {
         this.name = name;
         this.licenseNumber = licenseNumber;
+    }
+
+    public Driver(Long id, String name, String licenseNumber, String login, String password) {
+        this.id = id;
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Driver(String name, String licenseNumber, String login, String password) {
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+        this.login = login;
+        this.password = password;
     }
 
     public Long getId() {
@@ -39,6 +56,22 @@ public class Driver {
         this.licenseNumber = licenseNumber;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -57,4 +90,5 @@ public class Driver {
     public int hashCode() {
         return Objects.hash(id, name, licenseNumber);
     }
+
 }
