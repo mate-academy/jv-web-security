a964444c3c36e7778fb570fbb033c066771d23fb
Sushko Roman <formatesus@gmail.com>
2023-04-09 16:06:24 +0300

    added method findByUsername to DriverService and DriverServiceImpl

--- src/main/java/taxi/service/DriverService.java
+++ src/main/java/taxi/service/DriverService.java
@@ -1,6 +1,9 @@
 package taxi.service;
 
+import java.util.Optional;
 import taxi.model.Driver;
 
 public interface DriverService extends GenericService<Driver> {
+    public Optional<Driver> findByUsername(String username);
+
 }
