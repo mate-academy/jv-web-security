3fae3d3b78d02613496c2ecf21d5fad11f5cf104
KosMarch <kosmarchenko98@gmail.com>
2023-04-09 18:42:34 +0300

    fixed common mistakes

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -2,15 +2,14 @@ package taxi.service;
 
 import java.util.Optional;
 import taxi.exception.AuthenticationException;
-import taxi.lib.Injector;
+import taxi.lib.Inject;
 import taxi.lib.Service;
 import taxi.model.Driver;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
-    private static final Injector injector = Injector.getInstance("taxi");
-    private final DriverService driverService = (DriverService) injector
-            .getInstance(DriverService.class);
+    @Inject
+    private DriverService driverService;
 
     @Override
     public Driver login(String username, String password) throws AuthenticationException {
