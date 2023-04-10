9586a171c200f92bc2dde0f64dfdb19b1e6105a5
Oleksandr Lutsenko <sashagrek03@gmail.com>
2023-04-10 20:54:13 +0300

    fix a little

--- src/main/java/taxi/web/filter/AuthenticationFilter.java
+++ src/main/java/taxi/web/filter/AuthenticationFilter.java
@@ -14,12 +14,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class AuthenticationFilter implements Filter {
-    private Set<String> urls = new HashSet<>();
+    private Set<String> allowedUrls = new HashSet<>();
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        urls.add("/login");
-        urls.add("/drivers/add");
+    public void init(FilterConfig filterConfig) {
+        allowedUrls.add("/login");
+        allowedUrls.add("/drivers/add");
     }
 
     @Override
@@ -30,7 +30,7 @@ public class AuthenticationFilter implements Filter {
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         HttpSession session = req.getSession();
         Long driverId = (Long) session.getAttribute("driver_id");
-        if (driverId == null && !(urls.contains(req.getServletPath()))) {
+        if (driverId == null && !(allowedUrls.contains(req.getServletPath()))) {
             resp.sendRedirect(req.getContextPath() + "/login");
             return;
         }
