022917c7ffd09a23b88537d6fad991aa47e6a567
Oleh Sarapuk <olehsarapuk@gmail.com>
2023-04-10 14:29:04 +0300

    added checkstyle fixes

--- src/main/java/taxi/controller/LogoutController.java
+++ src/main/java/taxi/controller/LogoutController.java
@@ -5,7 +5,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import taxi.lib.Injector;
 
 public class LogoutController extends HttpServlet {
     @Override
