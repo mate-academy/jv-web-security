23f2f5dd320716eb4fbb52e2389beb63a2053b6a
IvanZuravlev228 <courses.zhuravlev.ivan@gmail.com>
2023-04-10 17:31:30 +0300

    fixed checkstyle mistake

--- src/main/java/taxi/controller/LoginController.java
+++ src/main/java/taxi/controller/LoginController.java
@@ -30,7 +30,7 @@ public class LoginController extends HttpServlet {
                     req.getParameter("login"), req.getParameter("password"));
             HttpSession session = req.getSession();
             session.setAttribute("driver_id", driver.getId());
-            resp.sendRedirect("/index");
+            resp.sendRedirect(req.getContextPath() + "/index");
         } catch (AuthenticationException e) {
             req.setAttribute("errorMsg", e.getMessage());
             req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
