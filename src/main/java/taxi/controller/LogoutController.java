fec6863d09880427804d522e6f2b339190f653b1
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-04-10 18:57:04 +0300

    logout fix

--- src/main/java/taxi/controller/LogoutController.java
+++ src/main/java/taxi/controller/LogoutController.java
@@ -11,6 +11,5 @@ public class LogoutController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         req.getSession().invalidate();
-        req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
     }
 }
