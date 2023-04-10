c3274611c66bc5cb06a1c6ea16170849dee40d4e
Artem Grunin <artem.grunin.2006@gmail.com>
2023-04-10 11:31:44 +0300

    deleted empty line in AuthenticationService

--- src/main/java/taxi/service/AuthenticationService.java
+++ src/main/java/taxi/service/AuthenticationService.java
@@ -4,6 +4,5 @@ import taxi.exception.AuthenticationException;
 import taxi.model.Driver;
 
 public interface AuthenticationService {
-
     Driver login(String login, String password) throws AuthenticationException;
 }
