070227e5067b6c1cc974d85b9fa8448b03f6379b
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-04-10 18:11:41 +0300

    Fixed login display error, fixed GetMyCurrentCarsController.

--- src/main/java/taxi/controller/driver/GetMyCurrentCarsController.java
+++ src/main/java/taxi/controller/driver/GetMyCurrentCarsController.java
@@ -25,7 +25,7 @@ public class GetMyCurrentCarsController extends HttpServlet {
         Long driverId = (Long) session.getAttribute("driver_id");
         List<Car> cars = carService.getAllByDriver(driverId);
         req.setAttribute("cars", cars);
-        req.getRequestDispatcher("WEB-INF/views/cars/all").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
     }
 
 }
--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 import taxi.dao.DriverDao;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Inject;
+import taxi.lib.Service;
 import taxi.model.Driver;
 
+@Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
     private DriverDao driverDao;
