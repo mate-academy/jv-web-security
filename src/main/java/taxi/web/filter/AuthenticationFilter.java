0dfd819c9510de33c0dcb8f213be77570a5a68e2
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-04-28 18:17:42 +0300

    checkstyle done

--- src/main/java/taxi/web/filter/AuthenticationFilter.java
+++ src/main/java/taxi/web/filter/AuthenticationFilter.java
@@ -13,8 +13,6 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import taxi.lib.Injector;
-import taxi.service.DriverService;
 
 @WebFilter(urlPatterns = "/*")
 public class AuthenticationFilter implements Filter {
