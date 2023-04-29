a68da8654bb75e15ebd73f2b137475b8797f0ac1
Artur Muliukov <arthur.muliukov@gmail.com>
2023-04-30 00:03:59 +0200

    Solved 1.4

--- src/main/java/taxi/controller/driver/cars/GetMyCurrentCarsController.java
+++ src/main/java/taxi/controller/driver/cars/GetMyCurrentCarsController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 import taxi.lib.Injector;
 import taxi.model.Car;
 import taxi.service.CarService;
