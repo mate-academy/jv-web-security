a1690309078210fafd04e78270c69440bdcebb11
Trach Vika <vikaa@Oksanas-MacBook-Pro.local>
2023-06-15 23:31:43 +0300

    hw-jv-web-security

--- src/main/java/taxi/dao/CarDaoImpl.java
+++ src/main/java/taxi/dao/CarDaoImpl.java
@@ -188,8 +188,7 @@ public class CarDaoImpl implements CarDao {
     }
 
     private List<Driver> getAllDriversByCarId(Long carId) {
-        String query = "SELECT id, name, license_number "
-                + "FROM cars_drivers cd "
+        String query = "SELECT id, name, license_number, login, password "
                 + "JOIN drivers d ON cd.driver_id = d.id "
                 + "WHERE car_id = ? AND is_deleted = false";
         try (Connection connection = ConnectionUtil.getConnection();
@@ -212,9 +211,13 @@ public class CarDaoImpl implements CarDao {
         String name = resultSet.getString("name");
         String licenseNumber = resultSet.getString("license_number");
         Driver driver = new Driver();
+        String login = resultSet.getString("login");
+        String password = resultSet.getString("password");
         driver.setId(driverId);
         driver.setName(name);
         driver.setLicenseNumber(licenseNumber);
+        driver.setLogin(login);
+        driver.setPassword(password);
         return driver;
     }
 
---
+++ src/main/webapp/WEB-INF/views/header.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: vikaa
+  Date: 15.06.2023
+  Time: 20:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<a href="${pageContext.request.contextPath}/logout">Logout</a>
+</body>
+</html>
