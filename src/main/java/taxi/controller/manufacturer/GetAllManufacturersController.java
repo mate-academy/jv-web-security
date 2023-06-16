66b0bc848cdbfaa8b159ff59ff12060c9c9cfd78
pavlokhomiak <pavlo.khomiak93@gmail.com>
2023-06-16 18:25:43 +0300

    solution

--- src/main/java/taxi/controller/manufacturer/GetAllManufacturersController.java
+++ src/main/java/taxi/controller/manufacturer/GetAllManufacturersController.java
@@ -1,11 +1,11 @@
 package taxi.controller.manufacturer;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import taxi.lib.Injector;
 import taxi.model.Manufacturer;
 import taxi.service.ManufacturerService;
