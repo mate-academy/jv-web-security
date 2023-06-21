c612f33cf2fa0e52e62b781fa3d08bf95f3329dd
Azzzrael <vasyl.gladky@gmail.com>
2023-06-21 21:36:20 +0300

    web-security task done

---
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -0,0 +1,26 @@
+package taxi.service;
+
+import java.util.Optional;
+import taxi.exception.AuthentificationException;
+import taxi.lib.Injector;
+import taxi.lib.Service;
+import taxi.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private DriverService driverService
+            = (DriverService) injector.getInstance(DriverService.class);
+
+    @Override
+    public Driver login(String login, String password) throws AuthentificationException {
+        Optional<Driver> driver = driverService.findByLogin(login);
+        if (driver.isEmpty()) {
+            throw new AuthentificationException("username or password was incorrect");
+        }
+        if (driver.get().getPassword().equals(password)) {
+            return driver.get();
+        }
+        throw new AuthentificationException("username or password was incorrect");
+    }
+}
