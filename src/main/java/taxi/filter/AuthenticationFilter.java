4c1dec491b58fdc40b18a2412b07eed9cc6503d9
Chris <tynkastya@gmail.com>
2023-06-27 20:30:39 +0300

    implemented task 3

--- src/main/java/taxi/filter/AuthenticationFilter.java
+++ src/main/java/taxi/filter/AuthenticationFilter.java
@@ -1,4 +1,4 @@
-package taxi.controller.filter;
+package taxi.filter;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -31,11 +31,7 @@ public class AuthenticationFilter implements Filter {
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         HttpSession session = req.getSession();
         Long driverId = (Long) session.getAttribute("driver_id");
-        if (driverId == null && allowedUrls.contains(req.getServletPath())) {
-            filterChain.doFilter(req, resp);
-            return;
-        }
-        if (driverId == null) {
+        if (driverId == null && !allowedUrls.contains(req.getServletPath())) {
             resp.sendRedirect(req.getContextPath() + "/login");
             return;
         }
