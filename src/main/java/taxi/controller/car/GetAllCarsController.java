d1ba4d14fc811cca9c45fd0045e54f16ba40d8b4
Chris <tynkastya@gmail.com>
2023-06-29 10:01:59 +0300

    implemented task 5

--- src/main/java/taxi/controller/car/GetAllCarsController.java
+++ src/main/java/taxi/controller/car/GetAllCarsController.java
@@ -19,6 +19,6 @@ public class GetAllCarsController extends HttpServlet {
             throws ServletException, IOException {
         List<Car> cars = carService.getAll();
         req.setAttribute("cars", cars);
-        req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/cars/get.jsp").forward(req, resp);
     }
 }
