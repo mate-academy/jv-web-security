b20af95f1cbdd46e2e958468765d2eed9d0b87d9
Pavlo Polezhay <pasha200417@gmail.com>
2023-06-29 11:41:47 +0300

    Solved

--- src/main/java/taxi/model/Driver.java
+++ src/main/java/taxi/model/Driver.java
@@ -4,6 +4,8 @@ import java.util.Objects;
 
 public class Driver {
     private Long id;
+    private String login;
+    private String password;
     private String name;
     private String licenseNumber;
 
@@ -13,6 +15,17 @@ public class Driver {
     public Driver(String name, String licenseNumber) {
         this.name = name;
         this.licenseNumber = licenseNumber;
+    }
+
+    public Driver(String name, String licenseNumber, String login, String password) {
+        this(name, licenseNumber);
+        this.login = login;
+        this.password = password;
+    }
+
+    public Driver(Long id, String login, String password, String name, String licenseNumber) {
+        this(name, licenseNumber, login, password);
+        this.id = id;
     }
 
     public Long getId() {
@@ -37,6 +50,31 @@ public class Driver {
 
     public void setLicenseNumber(String licenseNumber) {
         this.licenseNumber = licenseNumber;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{"
+                + "id=" + id
+                + ", login='" + login + '\''
+                + ", name='" + name + '\''
+                + ", licenseNumber='" + licenseNumber + '\'' + '}';
     }
 
     @Override
