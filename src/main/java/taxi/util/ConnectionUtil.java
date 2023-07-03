d36366b92a936e7b51ee2af8dacfb2adbe1bc209
Volodymyr Rusyn <volodymyrruso@gmail.com>
2023-07-03 13:30:43 +0200

    commit AuthService, Filter, GetMyCurrentCar, Login and Logout controllers,

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "12345";
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "YOUR_DB_URL";
+    private static final String USERNAME = "USERNAME";
+    private static final String PASSWORD = "PASSWORD";
+    private static final String JDBC_DRIVER = "YOUR_DRIVER";
 
     static {
         try {
