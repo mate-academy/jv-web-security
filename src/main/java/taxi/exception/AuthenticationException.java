bccfeb237cc17d44768a1bdff1db4fe2ffb77a51
Mykola Panasiuk <mikola.panasyuk.01@gmail.com>
2023-07-03 10:32:09 +0300

    moved the implementation of services to the appropriate directory 'impl'

--- src/main/java/taxi/exception/AuthenticationException.java
+++ src/main/java/taxi/exception/AuthenticationException.java
@@ -1,6 +1,6 @@
 package taxi.exception;
 
-public class AuthenticationException extends RuntimeException {
+public class AuthenticationException extends Exception {
     public AuthenticationException(String message) {
         super(message);
     }
