f09ce685985a7deb45ebbac9c1df5e5d10c0c12c
Mykyta Naumov <nneed4use@gmail.com>
2023-07-04 18:13:21 +0300

    Partially done

--- src/main/java/taxi/controller/driver/GetMyCurrentCarsController.java
+++ src/main/java/taxi/controller/driver/GetMyCurrentCarsController.java
@@ -3,7 +3,6 @@ package taxi.controller.driver;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,6 @@ import taxi.lib.Injector;
 import taxi.model.Car;
 import taxi.service.CarService;
 
-@WebServlet ("/myCars")
 public class GetMyCurrentCarsController extends HttpServlet {
     private final Injector injector = Injector.getInstance("taxi");
     private final CarService carService = (CarService) injector.getInstance(CarService.class);
--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -15,12 +15,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
         Optional<Driver> driver = driverDao.findByLogin(login);
-        if (driver.isEmpty()) {
-            throw new AuthenticationException("Login was incorrect");
-        }
-        if (driver.get().getPassword().equals(password)) {
+        if (driver.isPresent() && driver.get().getPassword().equals(password)) {
             return driver.get();
         }
-        throw new AuthenticationException("Password was incorrect");
+        throw new AuthenticationException("Login or password was incorrect");
     }
 }
