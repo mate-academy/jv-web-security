93d926773e8df5193fa08db62a84850799033fb5
AndriiBohun <poikka69@gmail.com>
2023-07-05 20:09:59 +0300

    jv-web-security_C07

--- src/main/java/taxi/web/filter/AuthenticationFilter.java
+++ src/main/java/taxi/web/filter/AuthenticationFilter.java
@@ -32,10 +32,7 @@ public class AuthenticationFilter implements Filter {
 
         Long driverId = (Long) session.getAttribute("driver_id");
 
-        if (driverId == null && allowedUrls.contains(req.getServletPath())) {
-            chain.doFilter(req, resp);
-            return;
-        } else if (driverId == null) {
+        if (driverId == null && !allowedUrls.contains(req.getServletPath())) {
             resp.sendRedirect("/login");
             return;
         }
