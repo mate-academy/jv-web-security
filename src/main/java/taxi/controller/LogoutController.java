506789b5c8a30890440f5b621ef24bbc0e8f1e25
Anton Uzhva <anton.itec91@gmail.com>
2023-07-30 19:15:20 +0300

    -amend

--- src/main/java/taxi/controller/LogoutController.java
+++ src/main/java/taxi/controller/LogoutController.java
@@ -19,6 +19,5 @@ public class LogoutController extends HttpServlet {
         HttpSession session = req.getSession();
         session.invalidate();
         resp.sendRedirect(req.getContextPath() + "/index");
-
     }
 }
