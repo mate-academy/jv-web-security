5987d7f1146ed83882d00532995a43651c2e70ac
Mykhailo <mykhailokuryk16@gmail.com>
2023-08-07 19:26:02 +0300

    add logger...

--- src/main/java/taxi/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/impl/AuthenticationServiceImpl.java
@@ -1,19 +1,25 @@
-package taxi.service;
+package taxi.service.impl;
 
 import java.util.Optional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import taxi.dao.DriverDao;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Inject;
 import taxi.lib.Service;
 import taxi.model.Driver;
+import taxi.service.AuthenticationService;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    public static final Logger logger =
+            LogManager.getLogger(AuthenticationServiceImpl.class);
     @Inject
     private DriverDao driverDao;
 
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
+        logger.info("Method login was called.Params: login = {}", login);
         Optional<Driver> driver = driverDao.findByLogin(login);
         if (driver.isPresent() && driver.get().getPassword().equals(password)) {
             return driver.get();
--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi?serverTimezone=UTC";
+    private static final String URL =
+            "jdbc:mysql://database-1.clt2weeraobx.eu-north-1.rds.amazonaws.com:3306/taxi";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "glorytobeServer";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
